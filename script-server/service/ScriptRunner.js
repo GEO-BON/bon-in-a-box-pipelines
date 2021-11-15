8be648211260b7ae3eddd158b39360578ca636fb
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2021-11-15 16:04:30 -0500

    Running a simple R script

---
+++ script-server/service/ScriptRunner.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+var scriptFolder = process.env.SCRIPT_LOCATION;
+
+const Fs = require('fs')  
+const Path = require('path')
+
+/**
+ * Run this script
+ * Run this script ... decription ...
+ *
+ * scriptPath String Where to find the script in ./script folder
+ * returns inline_response_200
+ **/
+exports.runScript = function (scriptPath) {
+  return new Promise(function (resolve, reject) {
+    const script = Path.join(scriptFolder, scriptPath)
+    
+    if (Fs.existsSync(script)  ) {
+      // This is a very basic way to run the script, we will probably change this.
+      const exec = require('child_process').exec;
+      const myShellScript = exec('Rscript ' + script);
+      myShellScript.stdout.on('data', (data)=>{
+          console.log(data); 
+      });
+      myShellScript.stderr.on('data', (data)=>{
+          console.error(data);
+      });
+ 
+      var examples = {};
+      examples['application/json'] = {
+        "file": "map.tiff",
+        "message": script + " ran!"
+      };
+      resolve(examples[Object.keys(examples)[0]])
+    } else {
+      console.log('Script not found: ' + script)
+      reject({
+        "message": "Script not found"
+      });
+    }
+  });
+}
+
