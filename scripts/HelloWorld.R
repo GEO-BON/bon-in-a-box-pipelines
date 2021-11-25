8ce3d21a0a8de926c587b33812cbcc895acf6579
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2021-11-25 13:40:14 -0500

    #52 Receiving an array of params

--- scripts/HelloWorld.R
+++ scripts/HelloWorld.R
@@ -1,10 +1,20 @@
 print("Hello World from R!")
-file.create("/output/hello_file.txt")
+
+# Environment variables available
 print(Sys.getenv("OUTPUT_LOCATION"))
 print(Sys.getenv("SCRIPT_LOCATION"))
 
-cat('
+# Receiving args
+args <- commandArgs(trailingOnly=TRUE)
+cat(args, sep = "\n")
+
+# Script body
+example_file = file.path(Sys.getenv("OUTPUT_LOCATION"), "hello_world.txt")
+file.create(example_file)
+
+# Outputing result
+cat(paste0('
 {
-    "presence":"map1.tiff",
+    "example_file":"',example_file,'",
     "uncertainty":"map2.tiff"
-}')
+}'))
