e37cffafe4ffda78d4ceb58b3f412a83b99c1ca1
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2021-11-26 13:40:55 -0500

    #53: Output to a json file.

--- scripts/HelloWorld.R
+++ scripts/HelloWorld.R
@@ -1,20 +1,26 @@
-print("Hello World from R!")
-
 # Environment variables available
-print(Sys.getenv("OUTPUT_LOCATION"))
 print(Sys.getenv("SCRIPT_LOCATION"))
+
+# Install missing packages
+packages <- c("rjson")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 # Receiving args
 args <- commandArgs(trailingOnly=TRUE)
+outputFolder <- args[1] # Arg 1 is always the output folder
 cat(args, sep = "\n")
 
 # Script body
-example_file = file.path(Sys.getenv("OUTPUT_LOCATION"), "hello_world.txt")
+example_file = file.path(outputFolder, "hello_world.txt")
 file.create(example_file)
 
-# Outputing result
-cat(paste0('
-{
-    "example_file":"',example_file,'",
-    "uncertainty":"map2.tiff"
-}'))
+# Outputing result to JSON
+output <- list(presence = example_file, 
+                uncertainty = "map2.tiff")
+                
+library("rjson")
+jsonData <- toJSON(output, indent=2)
+write(jsonData, file.path(outputFolder,"output.json"))
+
+# (If we get a problem with encoding, we could use utf-8 library to clean the output, since the server reads it as utf-8)
