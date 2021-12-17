48579c91e7f699b649e0e9fbc00391cfc0ebd5a6
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2021-12-17 15:05:56 -0500

    #17: Basic UI that gives response in an alert. + spinner

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,43 +1,87 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import spinner from './spinner.svg';
 import './App.css';
+const BonInABoxScriptService = require('bon_in_a_box_script_service');
+
+const RequestState = Object.freeze({"idle":1, "working":2, "done":3, "error":4})
 
 function App() {
+  const [requestState, setRequestState] = useState(RequestState.idle);
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={query}>Test script server</button>
+        <h1>BON in a Box v2 pre-pre-pre alpha</h1>
       </header>
-    </div>
+      <Form requestState={requestState} setRequestState={setRequestState} />
+      <Result requestState={requestState} />
+    </>
   );
 }
 
-function query() {
-  var BonInABoxScriptService = require('bon_in_a_box_script_service');
+function Form(props) {
 
-  var api = new BonInABoxScriptService.DefaultApi()
-  var scriptPath = "scriptPath_example"; // {String} Where to find the script in ./script folder
-  var callback = function(error, data, response) {
-    if (error) {
-      alert(error)
-      console.error(error);
-    } else {
-      alert('API called successfully. Returned data: ' + data)
-      console.log('API called successfully. Returned data: ' + data);
+  const queryInfo = () => {
+    props.setRequestState(RequestState.working)
+
+    var api = new BonInABoxScriptService.DefaultApi()
+    var scriptPath = "HelloWorld.R"; // {String} Where to find the script in ./script folder
+    var callback = function (error, data, response) {
+      if (error) {
+        props.setRequestState(RequestState.error)
+        console.error(error);
+        alert(error)
+
+      } else {
+        props.setRequestState(RequestState.done)
+        console.log('API called successfully. Returned data: ' + data);
+        alert('API called successfully. ' + response + ' Returned data: ' + data)
+      }
     }
-  };
-  api.getScriptInfo(scriptPath, callback);
+    api.getScriptInfo(scriptPath, callback);
+  }
+
+
+  const runScript = () => {
+    props.setRequestState(RequestState.working)
+
+    var api = new BonInABoxScriptService.DefaultApi()
+    var scriptPath = "HelloWorld.R"; // {String} Where to find the script in ./script folder
+    var callback = function (error, data, response) {
+      console.log('Got callback');
+      if (error) {
+        props.setRequestState(RequestState.error)
+        alert(error)
+        console.error(error);
+
+      } else {
+        props.setRequestState(RequestState.done)
+        console.log('API called successfully. Returned data: ' + data);
+        alert('API called successfully. ' + response + ' Returned data: ' + data)
+      }
+    };
+
+    console.log('Launching');
+    api.runScript(scriptPath, {"params":["test1", "test2"]}, callback);
+  }
+
+  return (
+    <>
+        <button onClick={runScript} disabled={props.requestState === RequestState.working}>Run script</button>
+        <button onClick={queryInfo} disabled={props.requestState === RequestState.working}>Get script info</button>
+    </>
+  );
 }
 
-export default App;
+function Result(props) {
+  if (props.requestState === RequestState.working) {
+    return (
+      <div>
+        <img src={spinner} className="spinner" alt="Spinner" />
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export default App
