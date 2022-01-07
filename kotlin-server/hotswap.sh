f0666085c7755303fda49ebd74c9e3a6a41f0e25
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-01-07 14:27:32 -0500

    #59: Generated kotlin server with openapi tools The goal is to see if it handles parameters better.

---
+++ kotlin-server/hotswap.sh
@@ -0,0 +1,1 @@
+docker exec -it biab-script-server-kotlin sh -c "cd /home/gradle/project/ && gradle build"
---
+++ kotlin-server/src/main/kotlin/org/openapitools/server/AppMain.kt
@@ -0,0 +1,40 @@
+package org.openapitools.server
+
+import com.codahale.metrics.Slf4jReporter
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.metrics.dropwizard.*
+import java.util.concurrent.TimeUnit
+import io.ktor.routing.*
+import io.ktor.util.*
+import org.openapitools.server.apis.ScriptRunner
+
+
+
+@KtorExperimentalAPI
+@KtorExperimentalLocationsAPI
+fun Application.main() {
+    install(DefaultHeaders)
+    install(DropwizardMetrics) {
+        val reporter = Slf4jReporter.forRegistry(registry)
+            .outputTo(log)
+            .convertRatesTo(TimeUnit.SECONDS)
+            .convertDurationsTo(TimeUnit.MILLISECONDS)
+            .build()
+        reporter.start(10, TimeUnit.SECONDS)
+    }
+    install(ContentNegotiation) {
+        register(ContentType.Application.Json, GsonConverter())
+    }
+    install(AutoHeadResponse) // see https://ktor.io/docs/autoheadresponse.html
+    install(Compression, ApplicationCompressionConfiguration()) // see https://ktor.io/docs/compression.html
+    install(HSTS, ApplicationHstsConfiguration()) // see https://ktor.io/docs/hsts.html
+    install(Locations) // see https://ktor.io/docs/features-locations.html
+    install(Routing) {
+        ScriptRunner(log)
+    }
+
+}
