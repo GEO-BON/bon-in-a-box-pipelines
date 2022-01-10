59f1b13b1e413de7b04d5bd10ae0dd21cf997857
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-01-10 09:56:33 -0500

    Kt server supports script info

--- kotlin-server/src/main/kotlin/org/openapitools/server/apis/ScriptRunner.kt
+++ kotlin-server/src/main/kotlin/org/openapitools/server/apis/ScriptRunner.kt
@@ -12,28 +12,31 @@ import org.openapitools.server.Paths
 import io.ktor.locations.*
 import io.ktor.routing.*
 import org.openapitools.server.infrastructure.ApiPrincipal
-import org.openapitools.server.models.InlineResponse200
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.openapitools.server.models.ScriptRunResult
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.File
 
+val scriptFolder = System.getenv("SCRIPT_LOCATION")
 
 @KtorExperimentalLocationsAPI
 fun Route.ScriptRunner(logger:Logger) {
-    val gson = Gson()
-    val empty = mutableMapOf<String, Any?>()
 
-    get<Paths.getScriptInfo> {
-        val exampleContentType = "text/plain"
-        val exampleContentString = """http://server.com/scripts/somescript.md"""
-        
-        
-        when (exampleContentType) {
-            "application/json" -> call.respond(gson.fromJson(exampleContentString, empty::class.java))
-            "application/xml" -> call.respondText(exampleContentString, ContentType.Text.Xml)
-            else -> call.respondText(exampleContentString)
+    get<Paths.getScriptInfo> { parameters ->
+        try {
+            // Replace extension by .md
+            val mdPath = parameters.scriptPath.replace(Regex("""\.\w+$"""), ".md")
+            val scriptFile = File(scriptFolder, mdPath)
+            call.respondText(scriptFile.readText())
+            logger.trace("200: getScriptInfo $scriptFile")
+        } catch (ex:Exception) {
+            call.respondText(text=ex.message!!, status=HttpStatusCode.NotFound)
+            logger.trace("Error 404: getScriptInfo ${parameters.scriptPath}")
         }
     }
 
+    val gson = Gson()
+    val empty = mutableMapOf<String, Any?>()
     get<Paths.runScript> { parameters ->
         val exampleContentType = "application/json"
         val exampleContentString = """{
