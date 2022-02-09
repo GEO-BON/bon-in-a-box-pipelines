0fee0f65acd3270537b0ad1913a95a649c82b861
SarahVal <sarah.valentin28@gmail.com>
2022-02-09 10:11:29 -0500

    Update loadGbif.R

--- scripts/loadGbif.R
+++ scripts/loadGbif.R
@@ -7,7 +7,7 @@ packages <- c("rgbif", "rjson", "raster", "dplyr")
 new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-## Install required packages
+## Load required packages
 library("rgbif")
 library("dplyr")
 library("raster")
@@ -34,7 +34,7 @@ gbifData <- occ_data(scientificName = input$species, hasCoordinate = T, limit=in
     data <- data.frame()
   } else {
     data <- data %>% dplyr::select(key, species, decimalLongitude, decimalLatitude, year, month, day, datasetName, basisOfRecord) %>%
-      dplyr::rename(id = key, scientific_name = species) %>%
+      dplyr::rename(id = key, scientificName = species) %>%
       mutate(created_by = 'GBIF')%>%
       mutate(id = as.double(id))
     if (nrow(data) == input$limit) {
@@ -42,15 +42,20 @@ gbifData <- occ_data(scientificName = input$species, hasCoordinate = T, limit=in
       
     }
   }
+  data <- test(a)
 #write.table(data, sprintf("%s/observationGbif.csv", getwd()),
  #            append = F, row.names = F, col.names = T, sep = ";")
  
  #output <- list("observation" =  sprintf("%s/observationGbif.csv", getwd())) 
   output <- list("species" = input$species,
                   "n.observations" =  nrow(data),
-                  "warning" = warning,
-                  "data" = head(data)
+                  "warning" = warning
+                  #,
+                 # "data" = head(data)
                   ) 
   jsonData <- toJSON(output, indent=2)
   write(jsonData, file.path(outputFolder,"output.json"))
   
+  test <- function(a) {
+    return(a)
+  }
