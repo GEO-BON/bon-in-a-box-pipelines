966439a1ce4740e4f399471a73f88a60b054c2a2
SarahVal <sarah.valentin28@gmail.com>
2022-02-15 09:34:33 -0500

    Create loadGbif.R

---
+++ scripts/loadGbif/loadGbif.R
@@ -0,0 +1,55 @@
+## Environment variables available
+# Script location can be used to access other scripts
+print(Sys.getenv("SCRIPT_LOCATION"))
+
+## Install required packages
+packages <- c("rgbif", "rjson", "raster", "dplyr")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## Load required packages
+library("rgbif")
+library("dplyr")
+library("raster")
+library("rjson")
+## Receiving args
+args <- commandArgs(trailingOnly=TRUE)
+outputFolder <- args[1] # Arg 1 is always the output folder
+cat(args, sep = "\n")
+
+
+input <- fromJSON(file=file.path(outputFolder, "input.json"))
+print("Inputs: ")
+print(input)
+
+# Loading data from GBIF (https://www.gbif.org/)
+warning <- ""
+gbifData <- occ_data(scientificName = input$species, hasCoordinate = T, limit=input$limit) 
+  data <- gbifData$data
+  
+  if (is.null(data)) {
+    warning <- sprintf("No observation found for species %s", species)
+    data <- data.frame()
+  } else {
+    data <- data %>% dplyr::select(key, species, decimalLongitude, decimalLatitude, year, month, day, datasetName, basisOfRecord) %>%
+      dplyr::rename(id = key, scientificName = species) %>%
+      mutate(created_by = 'GBIF')%>%
+      mutate(id = as.double(id))
+    if (nrow(data) == input$limit) {
+      warning <- "Number of observations equals the limit number. Some observations may be lacking."
+      
+    }
+  }
+
+#write.table(data, sprintf("%s/observationGbif.csv", getwd()),
+ #            append = F, row.names = F, col.names = T, sep = ";")
+ 
+ #output <- list("observation" =  sprintf("%s/observationGbif.csv", getwd())) 
+  output <- list("species" = input$species,
+                  "n.observations" =  nrow(data),
+                  "warning" = warning,
+                  "data" = data
+                  ) 
+  jsonData <- toJSON(output, indent=2)
+  write(jsonData, file.path(outputFolder,"output.json"))
+  
