183ce2c77c3f4830d19428bfd1ec898a6c2b8514
SarahVal <sarah.valentin28@gmail.com>
2022-02-16 12:20:49 -0500

    Update testStack.R

--- scripts/testStack.R
+++ scripts/testStack.R
@@ -23,3 +23,13 @@ print("Inputs: ")
 print(input)
 
  s <- stac(input$stac_path)
+ it_obj <- s |>
+  stac_search(bbox=c(-120,30,-40,68), collections=c('chelsa-clim'),limit=5000) |> get_request()
+
+st<-stac_image_collection(it_obj$features,asset_names=c("bio2"))
+
+print("st")
+v = cube_view(srs = "EPSG:32198",  extent = list(t0 = "1981-01-01", t1 = "1981-01-31",
+                                                 left = -2009488, right = 1401061,  top = 2597757, bottom = -715776),
+              dx = 2000, dy = 2000, dt = "P1Y",aggregation = "mean", resampling = "near")
+print("v")
