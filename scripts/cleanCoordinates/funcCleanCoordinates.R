c31733966ae7a130ae1f4a6e694e789f20ab3f6c
SarahVal <sarah.valentin28@gmail.com>
2022-03-02 17:28:35 -0500

    Update CleanCoordinates

--- scripts/cleanCoordinates/funcCleanCoordinates.R
+++ scripts/cleanCoordinates/funcCleanCoordinates.R
@@ -1,5 +1,6 @@
+
 #' @param x, a dataframe of observation containing at least: decimal coordinates into two columns and one unique identifier column 
-#' @param predictors, raster stack of predictors
+#' @param predictors, raster stack of predictors to remove NA.
 #' @param unique_id, column name containing a unique row identifier
 #' @param species_col, column name containing the species name
 #' @param lat, column name containing projected latitude, MUST BE same projection as the predictors raster
@@ -21,47 +22,53 @@
 
 
 cleanCoordinates <- function(x,
-                              predictors,
-                                 unique_id = "id",
-                                 lon = "lon", 
-                                 lat = "lat", 
-                              species_col = "scientific_name",
-                                 tests = c( 
-                                            "equal",
-                                            "zeros", 
-                                            "duplicates", 
-                                            "same_pixel",
-                                            "capitals", 
-                                            "centroids",
-                                            "seas", 
-                                            "urban",
-                                            "gbif", 
-                                            "institutions",
-                                            "env"
-                                            
-                                 ),
-                                 capitals_rad = 10000,
-                                 centroids_rad = 1000, 
-                                 centroids_detail = "both", 
-                                 inst_rad = 100, 
-                                 range_rad = 0,
-                                 zeros_rad = 0.5,
-                                threshold_env = 0.5,
+                             predictors = NULL,
+                             unique_id = "id",
+                             lon = "lon", 
+                             lat = "lat", 
+                             species_col = "scientificName",
+                             srs.obs,
+                             srs.target = NULL,
+                             spatial.res = NULL,
+                             tests = c( 
+                               "equal",
+                               "zeros", 
+                               "duplicates", 
+                               "same_pixel",
+                               "capitals", 
+                               "centroids",
+                               "seas", 
+                               "urban",
+                               "gbif", 
+                               "institutions",
+                               "env"
+                               
+                             ),
+                             capitals_rad = 10000,
+                             centroids_rad = 1000, 
+                             centroids_detail = "both", 
+                             inst_rad = 100, 
+                             range_rad = 0,
+                             zeros_rad = 0.5,
+                             threshold_env = 0.5,
                              predictors_env = NULL,
-                                 capitals_ref = NULL, 
-                                 centroids_ref = NULL, 
-                                 inst_ref = NULL, 
-                                 range_ref = NULL,
-                                 seas_ref = NULL, 
-                                 seas_scale = 10,
-                                 additions = NULL,
-                                 urban_ref = NULL, 
-                                 verbose = TRUE, 
-                                 species_name = NULL) {
+                             capitals_ref = NULL, 
+                             centroids_ref = NULL, 
+                             inst_ref = NULL, 
+                             range_ref = NULL,
+                             seas_ref = NULL, 
+                             seas_scale = 10,
+                             additions = NULL,
+                             urban_ref = NULL, 
+                             verbose = TRUE, 
+                             species_name = NULL) {
   # check function arguments
-  match.arg(value, choices = c("flagged", "clean"))
   match.arg(centroids_detail, choices = c("both", "country", "provinces"))
   
+  
+  if (!is.null(predictors)) {
+    srs.target <- crs(predictors)@projargs
+  } 
   
   # check column names
   nams <- c(unique_id, lon, lat, species_col)
@@ -71,66 +78,69 @@ cleanCoordinates <- function(x,
   
   # If proj is not lon/lat, transform coordinates to lon/lat to ensure further tests
   
-  x <- createDecimalCoordinates(x, lon = lon, lat = lat, crs_init = crs(predictors)@projargs)
+   x <- createDecimalCoordinates(x, lon = lon, lat = lat, crs_init = srs.obs)
   
   # Run tests Validity, check if coordinates fit to lat/long system, this has
   # to be run all the time, as otherwise the other tests don't work
-  val <- cc_val(x, lon = "decimalLon", lat =  "decimalLat", 
+  val <- CoordinateCleaner::cc_val(x, lon = "decimalLon", lat =  "decimalLat", 
                 verbose = verbose, value = "flagged")
   
   x1 <- x[!val,]
-
+  
   # For later test, only keep valid coordinates obs
   x <- x[val,]
   
-
+  
   
   ## Remove NA in predictors
   message("cleaning occurrences with no environmental data")
-  if (!class(predictors) %in% c("RasterBrick", "RasterStack")) {
+  if (!is.null(predictors)) {
     predictors <- raster::stack(predictors) # on utilise fonction subset du package raster sinon pb de comptaibitli
+    
+    presvals <- terra::extract(rast(predictors), dplyr::select(x, all_of(c(lon, lat ))) %>%
+                                 data.frame()) 
+    
+    
+    comp <-  complete.cases(presvals)
+    x <-  x[comp, ]
+    
+    if (nrow(x) == 0) stop("All occurrence points are outside the predictor variable rasters")
+    message(sprintf("Removed %s records with no environmental data.",nrow(presvals) - nrow(x)))
+    
   }
-
-  presvals <- terra::extract(rast(predictors), dplyr::select(x, all_of(c(lon, lat ))) %>%
-                                data.frame())
   
-  comp <-  complete.cases(presvals)
-  x <-  x[comp, ]
   
-  if (nrow(x) == 0) stop("All occurrence points are outside the predictor variable rasters")
-  message(sprintf("Removed %s records with no environmental data.",nrow(presvals) - nrow(x)))
-
   
-
+  
   
   # Initiate output 
-
+  
   allTests <- c( "val", "equ", "zer", "dup", "pixel", "cap", "cen", "sea", "urb",
-                      "gbf", "inst", "env")
+                 "gbf", "inst", "env")
   out <- data.frame(matrix(NA, nrow = nrow(x), ncol = length(allTests)))
   names(out) <- allTests
-
+  
   
   ## Equal coordinates
   if ("equal" %in% tests) {
     out$equ <- CoordinateCleaner::cc_equ(x,
-                      lon = "decimalLon", lat = "decimalLat", verbose = verbose, value = "flagged",
-                      test = "absolute"
+                                         lon = "decimalLon", lat = "decimalLat", verbose = verbose, value = "flagged",
+                                         test = "absolute"
     )
   }
   
   ## Zero coordinates
   if ("zeros" %in% tests) {
     out$zer <- CoordinateCleaner::cc_zero(x,
-                       lon = "decimalLon", lat = "decimalLat", buffer = zeros_rad, verbose = verbose,
-                       value = "flagged"
+                                          lon = "decimalLon", lat = "decimalLat", buffer = zeros_rad, verbose = verbose,
+                                          value = "flagged"
     )
   }
   
   ## Duplicates
   if ("duplicates" %in% tests) {
     out$dup <-CoordinateCleaner::cc_dupl(x, lon = "decimalLon" , lat = "decimalLat", species = species_col, additions = additions,
-                      value = "flagged")
+                                         value = "flagged")
     
   }
   
@@ -139,7 +149,18 @@ cleanCoordinates <- function(x,
     if (verbose) {
       message("Testing observations in the same pixel")
     }
-    mask <- predictors[[1]]
+
+    proj.pts <-projectCoords(x, lon = lon, lat = lat, proj.from = srs.obs, proj.to = srs.target)
+    x <- bind_cols(x,
+      setNames(data.frame(proj.pts), c("lon", "lat")))
+    if (!is.null(predictors)) {
+      mask <- predictors[[1]]
+    } else {
+      
+      mask <- raster(proj.pts, resolution = spatial.res)
+      
+    }
+    
     cell <- raster::cellFromXY(mask,  dplyr::select(x, lon, lat ) %>% data.frame())
     dup <- duplicated(cell)
     out$pixel <- !dup
@@ -153,8 +174,8 @@ cleanCoordinates <- function(x,
   ## Capitals
   if ("capitals" %in% tests) {
     out$cap <- CoordinateCleaner::cc_cap(x,
-                      lon = "decimalLon", lat = "decimalLat", buffer = capitals_rad, ref = capitals_ref,
-                      value = "flagged", verbose = verbose
+                                         lon = "decimalLongitude", lat = "decimalLatitude", buffer = capitals_rad, ref = capitals_ref,
+                                         value = "flagged", verbose = verbose
     )
   }
   
@@ -162,18 +183,18 @@ cleanCoordinates <- function(x,
   if ("centroids" %in% tests) {
     out$cen <- CoordinateCleaner::cc_cen(x,
                                          species = species_col,
-                      lon = "decimalLon", lat = "decimalLat", buffer = centroids_rad, test = centroids_detail,
-                      ref = countryref, value = "flagged", verbose = verbose
+                                         lon = "decimalLongitude", lat = "decimalLatitude", buffer = centroids_rad, test = centroids_detail,
+                                         ref = countryref, value = "flagged", verbose = verbose
     )
   }
   
   ## Seas
   if ("seas" %in% tests) {
     out$sea <- CoordinateCleaner::cc_sea(x,
-                      lon = "decimalLon", lat = "decimalLat", ref = seas_ref, 
-                      scale = seas_scale,
-                      verbose = verbose,
-                      value = "flagged"
+                                         lon = "decimalLongitude", lat = "decimalLatitude", ref = seas_ref, 
+                                         scale = seas_scale,
+                                         verbose = verbose,
+                                         value = "flagged"
     )
   }
   
@@ -181,30 +202,30 @@ cleanCoordinates <- function(x,
   if ("urban" %in% tests) {
     out$urb <- cc_urb(x,
                       
-                      lon = "decimalLon", lat = "decimalLat", ref = urban_ref, verbose = verbose,
+                      lon = "decimalLongitude", lat = "decimalLatitude", ref = urban_ref, verbose = verbose,
                       value = "flagged"
     )
   }
   
   ## GBIF headquarters
   if ("gbif" %in% tests) {
-    out$gbf <- CoordinateCleaner::cc_gbif(x, lon = "decimalLon", lat = "decimalLat", 
-                       verbose = verbose, value = "flagged")
+    out$gbf <- CoordinateCleaner::cc_gbif(x, lon = "decimalLongitude", lat = "decimalLatitude", 
+                                          verbose = verbose, value = "flagged")
   }
   
   ## Biodiversity institution
   if ("institutions" %in% tests) {
     out$inst <- CoordinateCleaner::cc_inst(x,
-                        lon = "decimalLon", lat = "decimalLat", ref = inst_ref, buffer = inst_rad,
-                        verbose = verbose, value = "flagged"
+                                           lon = "decimalLongitude", lat = "decimalLatitude", ref = inst_ref, buffer = inst_rad,
+                                           verbose = verbose, value = "flagged"
     )
   }
   
   ## Environmental outliers
   if ("env" %in% tests){
     out$env <- cc_env_out(x, 
-                          lon = lon, 
-                          lat = lat, 
+                          lon = "lon", 
+                          lat = "lat", 
                           predictors = predictors,
                           cols = predictors_env,
                           threshold = threshold_env,
@@ -213,10 +234,10 @@ cleanCoordinates <- function(x,
     )
   }
   
-    
+  
   # prepare output data
   
- 
+  
   
   if (nrow(x1) > 0) {
     x <- bind_rows(x1, x)
@@ -228,7 +249,7 @@ cleanCoordinates <- function(x,
     out <- out %>%
       mutate(val = T)
     out <- rbind(out_val,
-                     out)
+                 out)
   }
   out <- Filter(function(x) !all(is.na(x)), out)
   suma <- as.vector(Reduce("&", out))
@@ -263,61 +284,28 @@ cleanCoordinates <- function(x,
   
   
   
-flagged.obs <- ret %>% data.frame()
-clean.obs <- ret[suma, ] %>% dplyr::select(id, scientific_name, lon, lat) %>% data.frame()
-
-
+  flagged.obs <- ret %>% data.frame()
+  clean.obs <- ret[suma, ] %>% dplyr::select(id, scientific_name, lon, lat) %>% data.frame()
+  
+  
   return(list("flagged" = flagged.obs, "clean" = clean.obs, "report" = repo))
 }
 
-# Impoirted and modified from CoordinateCleaner
-cc_urb <- function (x, lon = "decimallongitude", lat = "decimallatitude", 
-                    ref = NULL, value = "clean", verbose = TRUE) {
-  match.arg(value, choices = c("clean", "flagged"))
-  if (verbose) {
-    message("Testing urban areas")
-  }
-  if (is.null(ref)) {
-    message("Downloading urban areas via rnaturalearth")
-    ref <- try(suppressWarnings(rnaturalearth::ne_download(scale = "medium", 
-                                                           type = "urban_areas")), silent = TRUE)
-    if (class(ref) == "try-error") {
-      warning(sprintf("Gazetteer for urban areas not found at\n%s", 
-                      rnaturalearth::ne_file_name(scale = "medium", 
-                                                  type = "urban_areas", full_url = TRUE)))
-      warning("Skipping urban test")
-      switch(value, clean = return(x), flagged = return(rep(NA, 
-                                                            nrow(x))))
-    }
-    sp::proj4string(ref) <- ""
-  }
-  else {
-    if (!any(is(ref) == "Spatial")) {
-      ref <- as(ref, "Spatial")
-    }
-    ref <- reproj(ref)
-  }
-  wgs84 <- "+proj=longlat +datum=WGS84 +no_defs"
-  dat <- sp::SpatialPoints(x[, c(lon, lat)], proj4string = CRS(wgs84))
-  limits <- raster::extent(dat) + 1
-  ref <- raster::crop(ref, limits)
-  
-  if (is.null(ref)) {
-    out <- rep(TRUE, nrow(x))
-  }
-  else {
-    proj4string(ref) <- wgs84
-    out <- is.na(sp::over(x = dat, y = ref)[, 1])
-  }
-  if (verbose) {
-    if (value == "clean") {
-      message(sprintf("Removed %s records.", sum(!out)))
-    }
-    else {
-      message(sprintf("Flagged %s records.", sum(!out)))
-    }
-  }
-  switch(value, clean = return(x[out, ]), flagged = return(out))
+
+#' @name projectCoords
+#' @param xy data frame, containing the coordinates to reproject
+#' @param lon string, name of the longitude column
+#' @param lat string, name of the latitude column
+#' @param proj.from character, initial projection of the xy coordinates
+#' @param proj.to character, target projection 
+#' @return spatial points in the proj.to projection
+
+projectCoords <- function(xy, lon = "lon", lat = "lat", proj.from, proj.to) {
+  xy <- dplyr::select(xy, all_of(c(lon, lat)))
+  sp::coordinates(xy) <-  c(lon, lat)
+  proj4string(xy) <- sp::CRS(proj.from)
+  xy <- spTransform(xy, sp::CRS(proj.to)) 
+  xy
 }
 
 
@@ -330,6 +318,6 @@ createDecimalCoordinates <- function(obs, lon, lat, crs_init) {
     setNames(c("decimalLon", "decimalLat"))
   #Suppress warnings: dplyr one_of creates a warning if column names do not exist
   suppressWarnings(obs <- obs %>%
-                     dplyr::select(-one_of(c("decimalLon", "decimalLat"))) %>% bind_cols(coordsDecimal))
+                     dplyr::select(-one_of(c("decimalLongitude", "decimalLatitude"))) %>% bind_cols(coordsDecimal))
   return(obs)
 }
