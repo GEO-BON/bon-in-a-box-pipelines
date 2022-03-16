308a1f11d647fcd07ae3f29cd7c8b7b8a541e7fc
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-03-16 12:10:51 -0400

    #176: Input file component to reuse with pipeline form

---
+++ ui/src/components/InputFileWithExample.js
@@ -0,0 +1,39 @@
+import React, { useRef, useEffect } from 'react';
+
+export function InputFileWithExample(props) {
+  const textareaRef = useRef(null);
+
+  /**
+   * Automatic horizontal and vertical resizing of textarea
+   * @param {textarea} input
+   */
+  function resize(input) {
+    input.style.height = "auto";
+    input.style.height = (input.scrollHeight) + "px";
+
+    input.style.width = "auto";
+    input.style.width = (input.scrollWidth) + "px";
+  }
+
+  useEffect(() => {
+    // Generate example input.json
+    let inputExamples = {};
+
+    let meta = props.scriptMetadata;
+    if (meta && meta.inputs) {
+      Object.keys(meta.inputs).forEach((inputKey) => {
+        let input = meta.inputs[inputKey];
+        if (input) {
+          const example = input.example;
+          inputExamples[inputKey] = example ? example : "...";
+        }
+      });
+    }
+
+    textareaRef.current.value = JSON.stringify(inputExamples, null, 2);
+    resize(textareaRef.current);
+  });
+
+  return <textarea ref={textareaRef} name="inputFile" className="inputFile" type="text" defaultValue={props.defaultValue}
+    onInput={(e) => resize(e.target)}></textarea>;
+}
