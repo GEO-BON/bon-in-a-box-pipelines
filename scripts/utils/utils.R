fbb84d64e3785dbe598955acb639466dc3200d85
SarahVal <sarah.valentin28@gmail.com>
2022-03-17 12:46:02 -0400

    Create utils.R

---
+++ scripts/utils/utils.R
@@ -0,0 +1,193 @@
+#' @param predictors, a raster, either from raster or terra format
+#' @param mask, a vector file, either from raster or terra format
+#' @return the predictors raster cropped and masked by mask, in terra format
+#' @import terra
+
+fast_crop <- function(predictors,
+                       mask) {
+  
+  # convert into terra raster for increased speed
+  if (!inherits(predictors, "SpatRaster")) {
+    predictors <- terra::rast(predictors)
+  }
+  
+  # convert into a SpatVector
+ if (!inherits(mask, "SpatVectors")) {
+    mask <- terra::vect(mask)
+  }
+predictors <- terra::crop(predictors, mask)
+  predictors <- terra::mask(predictors, mask, touches = FALSE)
+
+  # convert to raster format for later processing
+     return(predictors)
+}
+
+
+#' @name create_projection
+#' @param predictors, a raster, either from raster or terra format
+#' @param mask, a vector file, either from raster or terra format
+#' @return the predictors raster cropped and masked by mask, in terra format
+#' @import dplyr
+create_projection <- function(obs, lon, lat, proj.from, 
+ proj.to, new.lon = NULL, new.lat = NULL) {
+  
+  if(is.null(new.lon)) {
+    new.lon <- lon
+  }
+  
+  if(is.null(new.lat)) {
+    new.lat <- lat
+  }
+  
+  new.coords <- project_coords(obs, lon, lat, proj.from, proj.to)
+  new.coords.df <- data.frame(new.coords)%>% 
+    setNames(c(new.lon, new.lat))
+  
+  suppressWarnings(obs <- obs %>%
+                     dplyr::select(-one_of(c(new.lon, new.lat))) %>% dplyr::bind_cols(new.coords.df))
+  
+  return(obs)
+}
+
+
+#' @name project_coords
+#' @param xy data frame, containing the coordinates to reproject
+#' @param lon string, name of the longitude column
+#' @param lat string, name of the latitude column
+#' @param proj.from character, initial projection of the xy coordinates
+#' @param proj.to character, target projection
+#' @import sp dplyr
+#' @return spatial points in the proj.to projection
+
+project_coords <- function(xy, lon = "lon", lat = "lat", proj.from, proj.to = NULL) {
+  xy <- dplyr::select(xy, dplyr::all_of(c(lon, lat)))
+  sp::coordinates(xy) <-  c(lon, lat)
+  sp::proj4string(xy) <- sp::CRS(proj.from)
+
+  if (!is.null(proj.to)) {
+  xy <- sp::spTransform(xy, sp::CRS(proj.to)) 
+
+  }
+  xy
+}
+
+
+#' @name points_to_bbox
+#' @param xy data frame, containing the coordinates to reproject
+#' @param buffer integer, buffer to add around the observations
+#' @param proj.from character, initial projection of the xy coordinates
+#' @param proj.to character, target projection 
+#' @return a box extent
+points_to_bbox <- function(xy, buffer = 0, proj.from = NULL, proj.to = NULL) {
+  if (class(xy) != "SpatialPoints") {
+    sp::coordinates(xy) <- colnames(xy)
+    proj4string(xy) <- sp::CRS(proj.from)
+  }
+  bbox <-  sf::st_buffer(sf::st_as_sfc(sf::st_bbox(xy)), dist =  buffer)
+  
+  if (!is.null(proj.to) ) {
+    bbox <- bbox  %>%
+      sf::st_transform(crs = sp::CRS(proj.to))
+  }
+  bbox <- c(sf::st_bbox(bbox)$xmin, sf::st_bbox(bbox)$xmax,
+            sf::st_bbox(bbox)$ymin, sf::st_bbox(bbox)$ymax)
+  bbox
+}
+
+
+bbox_to_wkt <- function(xmin = NA, ymin = NA, xmax = NA, ymax = NA, bbox = NULL) {
+  if (is.null(bbox)) bbox <- c(xmin, ymin, xmax, ymax)
+  stopifnot(is.numeric(as.numeric(bbox)))
+  bbox_template <- 'POLYGON((%s %s,%s %s,%s %s,%s %s,%s %s))'
+  sprintf(bbox_template, 
+          bbox[1], bbox[2],
+          bbox[3], bbox[2],
+          bbox[3], bbox[4],
+          bbox[1], bbox[4],
+          bbox[1], bbox[2]
+  )
+}
+
+shp_to_bbox <- function(shp, proj.from = NULL, proj.to = NULL) {
+  if(is.na(sf::st_crs(shp)) && is.null(proj.from)) {
+    stop("proj.fom is null and shapefile has no crs.")
+  }
+  
+  if(is.na(sf::st_crs(shp))) {
+    crs(study_extent) <- proj.from
+    study_extent <- study_extent %>% sf::st_set_crs(proj.from)
+  }
+  
+  if (!is.null(proj.to) ) {
+    study_extent <- study_extent %>%
+      sf::st_transform(crs = sp::CRS(proj.to))
+  }
+  
+  
+  bbox <- sf::st_bbox(study_extent, crs = proj)
+
+  bbox
+}
+
+
+
+
+
+#' @name create_density_plots
+#' @param xy data frame, containing the coordinates to reproject
+#' @param lon string, name of the longitude column
+#' @param lat string, name of the latitude column
+#' @param proj.from character, initial projection of the xy coordinates
+#' @param proj.to character, target projection
+#' @import ggplot2
+#' @return spatial points in the proj.to projection
+#' @export
+create_density_plots <- function(df, factors = NULL, export = T, path = "./density_plot.pdf") {
+
+  df <- df %>% dplyr::mutate_at(.vars = factors, factor)
+
+  i <- 1
+  a <- list()
+  for (var in names(df)[-which(names(df) %in% c("pa", "lon", "lat", factors))]) {
+    
+    a[[i]] <- plot_density_cont(df, var)
+    i <- i + 1
+
+  }
+  j <- 1
+  for (f in factors) {
+    
+    a[[length(a)+j]] <- plot_density_cat(df, f)
+    
+  }
+
+  p <- do.call(ggpubr::ggarrange, c(a[1:length(a)],  ncol = 3, nrow = 4))
+
+  if (export){
+  ggsave(path)
+  }
+
+  return(p)
+}
+
+
+plot_density_cont <- function(df, var) {
+  vars <- c("pa", var)
+  df <- df %>% dplyr::select(dplyr::all_of(vars))
+  df <- df[complete.cases(df), ]       
+  
+  mu <- df %>%
+    dplyr::group_by(pa) %>%
+    dplyr::summarise_at(all_of(var), list(name = mean))
+  
+  
+  p <- ggplot2::ggplot(df, aes(x = .data[[var]], fill = pa)) +
+    ggplot2::geom_density(alpha = 0.4)
+  # Add mean lines
+  p <- p + ggplot2::geom_vline(data = mu, aes(xintercept = name, color = pa),
+                   linetype = "dashed")
+  
+  return(p)
+  
+}
+
