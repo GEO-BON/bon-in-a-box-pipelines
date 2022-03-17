4332fc8bf2425d0c168242d3d8b65b4ea8085cd4
SarahVal <sarah.valentin28@gmail.com>
2022-03-17 12:47:01 -0400

    Update cleanOccurrences module

--- scripts/cleanCoordinates/modCleanOccurrences.R
+++ scripts/cleanCoordinates/modCleanOccurrences.R
@@ -1,19 +1,9 @@
 
 
 ## Install required packages
-#packages <- c("terra", "rjson", "raster", "dplyr", "CoordinateCleaner")
-#new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-#if(length(new.packages)) install.packages(new.packages)
-
-#install.packages("devtools")
-#install.packages("Rtools")
-#library("remotes")
-# WorldClimTiles not in CRAN
-#library("devtools")
-#library("crul")
-#library("remotes")
-#options(timeout = max(60000000, getOption("timeout")))
-#remotes::install_github("kapitzas/WorldClimTiles")
+packages <- c("terra", "rjson", "raster", "dplyr", "CoordinateCleaner")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 ## Load required packages
 library("terra")
@@ -21,11 +11,12 @@ library("rjson")
 library("raster")
 library("CoordinateCleaner")
 library("dplyr")
-#library("WorldClimTiles")
+
 
 ## Load functions
-#source(paste(Sys.getenv("SCRIPT_LOCATION"), "funcCleanCoordinates.R", sep = "/"))
-source("/scripts/cleanCoordinates/funcCleanCoordinates.R")
+source(paste(Sys.getenv("SCRIPT_LOCATION"), "cleanCoordinates/funcCleanCoordinates.R", sep = "/"))
+source("/scripts/utils/utils.R")
+#source("/scripts/cleanCoordinates/funcCleanCoordinates.R")
 ## Receiving args
 args <- commandArgs(trailingOnly=TRUE)
 outputFolder <- args[1] # Arg 1 is always the output folder
@@ -38,60 +29,48 @@ print(input)
 
 obs <- read.table(file = input$obs, sep = '\t', header = TRUE) 
 
-#country_boundary <- raster::getData("GADM", country = "CAN", level = 1, download=TRUE, path=outputFolder) #
-#quebec_boundary <- country_boundary[country_boundary$NAME_1 == "Québec",]
-  
- # subDir <- file.path(outputFolder)
+if (is.null(input$predictors.dir) || input$predictors.dir == "...") {
 
- # box_extent_bioclim <- WorldClimTiles::tile_name(quebec_boundary, "worldclim") # determine which WorldClim tiles your study area intersects with.
+predictors <- NULL
+proj.to <- input$srs
 
- # clim_tiles <- tile_get(box_extent_bioclim, name =  "worldclim", var="tmean", path = subDir) # for 0.5 arcmin worldclim tiles of 
-  # predictors <- tile_merge(clim_tiles)
+} else {
+
+predictors <- loadPredictors(input$predictors.dir,
+                           subsetLayers = NULL,
+                           removeCollinear = F,
+                           loadNonCollinear = F) 
+proj.to <- terra::crs(predictors, proj = T)
+
+}
+
+#obs <- create_projection(presence, lon = "decimalLongitude", lat = "decimalLatitude", 
+#proj.from = input$srs.obs, proj.to = proj, new.lon = "lon", new.lat = "lat") 
 
 
-
-cleaningRes <-  cleanCoordinates(obs,
-                              predictors = NULL,
+cleaningRes <-  clean_coordinates(obs,
+                              predictors = predictors,
                                  unique_id = "id",
-                                 lon = "decimalLongitude", 
-                                 lat = "decimalLatitude", 
-                              species_col = "scientificName",
-                              srs.obs = input$srs.obs,
-                              srs.target = input$srs.target,
+                                 lon = "lon", 
+                                 lat = "lat", 
+                              species_col = "scientific_name",
+                              srs = proj.to,
+                              covars = input$covars,
                               spatial.res = input$spatial.res,
-                                 tests = c( 
-                                            "equal",
-                                            "zeros", 
-                                            "duplicates", 
-                                            "same_pixel"
-                                          #  "centroids",
-                                         #   "seas", 
-                                          #  "urban",
-                                          #  "gbif", 
-                                         #   "institutions"
-                                 ),
+                                 tests = input$tests,
                                  capitals_rad = 10000,
                                  centroids_rad = 1000, 
                                  centroids_detail = "both", 
                                  inst_rad = 100, 
                                  range_rad = 0,
                                  zeros_rad = 0.5,
-                                threshold_env = 0.5,
-                             predictors_env = NULL,
-                                 capitals_ref = NULL, 
-                                 centroids_ref = NULL, 
-                                 inst_ref = NULL, 
-                                 range_ref = NULL,
-                                 seas_ref = NULL, 
-                                 seas_scale = 10,
-                                 additions = NULL,
-                                 urban_ref = NULL, 
-                                 verbose = TRUE)
+                                threshold_env = 0.8,
+                                output_dir = outputFolder)
  
  #output <- list("observation" =  sprintf("%s/observationGbif.csv", getwd())) 
-  output <- list(
-                  "n.clean" =  nrow(cleaningRes$flagged),
-                  "n.flagged" =  nrow(cleaningRes$clean)
+  output <- list("n.observations" =  nrow(obs)
+                 "n.clean" =  nrow(cleaningRes$clean),
+                  
                   ) 
 
 jsonData <- toJSON(output, indent=2)
