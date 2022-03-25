6ab8c81c6d9b4e8bccf56106de607a0fec1c6b87
Guillaume Larocque <glaroc@gmail.com>
2022-03-25 13:12:00 -0400

    Fix getting data from GBIF PC

--- scripts/pyLoadObservations/pyLoadObservations.sh
+++ scripts/pyLoadObservations/pyLoadObservations.sh
@@ -1,3 +1,3 @@
 #!bin/sh
 cd /scripts/pyLoadObservations/docker/
-docker-compose run pc-dock python -u /home/jovyan/scripts/pyLoadObservations/getocc/get_occ.py ${1}
+docker-compose -f docker-compose.yaml run pc-dock  python -u /home/jovyan/scripts/pyLoadObservations/getocc/get_occ.py ${1}
