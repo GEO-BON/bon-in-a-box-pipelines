cd5860f5ddb667d03fedb5fa873393b65b358928
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-04-01 16:59:51 -0400

    Imported drag & drop POC into our layout. There are quite a few styling issues.

---
+++ ui/src/Layout.js
@@ -0,0 +1,37 @@
+import './Layout.css';
+
+import React from 'react'
+
+import { NavLink } from "react-router-dom";
+
+import BiaBLogo from "./img/boninabox.jpg"
+
+export function Layout(props) {
+
+  return (
+    <>
+      <div className="left-banner">
+        <img id="logo" src={BiaBLogo} alt="BON in a Box logo" />
+        {props.left}
+      </div>
+
+      <div className='right'>
+        <header className="App-header">
+          <h1>BON in a Box v2 pre-pre alpha</h1>
+        </header>
+
+        <nav>
+          <NavLink to="/script-form">Single script run</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/pipeline-form">Pipeline run</NavLink>
+          &nbsp;|&nbsp;
+          <NavLink to="/pipeline-editor">Pipeline editor</NavLink>
+        </nav>
+
+        <main>
+          {props.right}
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ ui/src/components/PipelineEditor/ScriptChooser.js
@@ -0,0 +1,26 @@
+import './Editor.css'
+
+import React from 'react';
+
+
+export default () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <aside className='scriptChooser'>
+      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+    </aside>
+  );
+};
