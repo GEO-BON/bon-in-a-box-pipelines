be423dee801166216c09341cf80d9af8d96f02ab
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-04-04 11:09:31 -0400

    Main content auto resize for height. Nothing's simlpe with height...

---
+++ ui/src/states/WindowDimensions.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+// Adapted from QoP's answer here https://stackoverflow.com/a/36862446/3519951
+function getWindowDimensions() {
+  const { innerWidth: windowWidth, innerHeight: windowHeight } = window;
+  return {
+    windowWidth,
+    windowHeight
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  console.log(windowDimensions)
+  useEffect(() => {
+    function handleResize() {
+      
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
