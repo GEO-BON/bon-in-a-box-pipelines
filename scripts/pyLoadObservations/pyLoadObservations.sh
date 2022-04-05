e64300b5f4c93e800c58de1e5e65f9effa2daca8
Guillaume Larocque <glaroc@gmail.com>
2022-04-05 17:13:59 -0400

    Test Docker without cd

--- scripts/pyLoadObservations/pyLoadObservations.sh
+++ scripts/pyLoadObservations/pyLoadObservations.sh
@@ -1,3 +1,2 @@
 #!bin/sh
-cd /scripts/pyLoadObservations/docker/
-docker-compose -f docker-compose.yaml run pc-dock  python -u /home/jovyan/scripts/pyLoadObservations/getocc/get_occ.py ${1}
+docker-compose -f /scripts/pyLoadObservations/docker/docker-compose.yaml run pc-dock python -u /home/jovyan/scripts/pyLoadObservations/getocc/get_occ.py ${1}
