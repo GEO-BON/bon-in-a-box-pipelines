ab9bac3b2e8a7adea9de89497132997186408960
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-04-07 14:03:25 -0400

    #184: Validation of the graph is done a postiori

--- script-server/src/main/kotlin/org/geobon/pipeline/Pipe.kt
+++ script-server/src/main/kotlin/org/geobon/pipeline/Pipe.kt
@@ -4,4 +4,8 @@ interface Pipe {
     val type:String
     suspend fun pull():Any
     fun dumpOutputFolders(allOutputs: MutableMap<String, String>)
+
+    fun validateGraph(): String {
+        return ""
+    }
 }
