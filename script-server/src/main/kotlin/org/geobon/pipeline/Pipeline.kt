c55eabede98cb4eed1182d959aabdc1295b91126
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2022-08-23 15:59:22 -0400

    #192: Inputs from input files linked with steps.

--- script-server/src/main/kotlin/org/geobon/pipeline/Pipeline.kt
+++ script-server/src/main/kotlin/org/geobon/pipeline/Pipeline.kt
@@ -9,8 +9,8 @@ import java.io.File
 
 val pipelinesRoot = File(System.getenv("PIPELINES_LOCATION"))
 
-class Pipeline(descriptionFile: File) {
-    constructor(relPath: String) : this(File(pipelinesRoot, relPath))
+class Pipeline(descriptionFile: File, inputs: String? = null) {
+    constructor(relPath: String, inputs: String? = null) : this(File(pipelinesRoot, relPath), inputs)
 
     companion object {
         private val logger: Logger = LoggerFactory.getLogger("Pipeline")
@@ -32,8 +32,8 @@ class Pipeline(descriptionFile: File) {
         val outputIds = mutableListOf<String>()
 
         // Load all nodes and classify them as steps, constants or pipeline outputs
-        val jsonObject = JSONObject(descriptionFile.readText())
-        jsonObject.getJSONArray(NODES_LIST).forEach { node ->
+        val pipelineJSON = JSONObject(descriptionFile.readText())
+        pipelineJSON.getJSONArray(NODES_LIST).forEach { node ->
             if (node is JSONObject) {
                 val id = node.getString(NODE__ID)
                 when (node.getString(NODE__TYPE)) {
@@ -75,7 +75,7 @@ class Pipeline(descriptionFile: File) {
                                     "float" -> nodeData.getFloat(NODE__DATA__VALUE)
                                     "boolean" -> nodeData.getBoolean(NODE__DATA__VALUE)
                                     // Everything else is read as text
-                                    else -> nodeData.optString(NODE__DATA__VALUE)
+                                    else -> nodeData.getString(NODE__DATA__VALUE)
                                 }
                             )
                         }
@@ -89,7 +89,7 @@ class Pipeline(descriptionFile: File) {
         }
 
         // Link steps & constants by reading the edges, and populate the pipelineOutputs variable
-        jsonObject.getJSONArray(EDGES_LIST).forEach { edge ->
+        pipelineJSON.getJSONArray(EDGES_LIST).forEach { edge ->
             if(edge is JSONObject) {
                 // Find the source pipe
                 val sourceId = edge.getString(EDGE__SOURCE_ID)
@@ -117,6 +117,39 @@ class Pipeline(descriptionFile: File) {
             }
         }
 
+        // Link inputs from the input file to the pipeline
+        inputs?.let {
+            val inputsJSON = JSONObject(inputs)
+            val inputsSpec = pipelineJSON.getJSONObject(INPUTS)
+            val regex = """([.>\w]+)@(\d+)\.(\w+)""".toRegex()
+            inputsJSON.keySet().forEach { key ->
+                val inputSpec = inputsSpec.optJSONObject(key)
+                    ?: throw RuntimeException ("Input received \"$key\" is not listed pipeline inputs. Listed inputs are ${inputsSpec.keySet()}")
+                val type = inputSpec.getString(INPUTS__TYPE)
+
+                val groups = regex.matchEntire(key)?.groups
+                    ?: throw RuntimeException("Input id \"$key\" is malformed")
+                //val path = groups[1]!!.value
+                val stepId = groups[2]!!.value
+                val inputId = groups[3]!!.value
+
+                val step = steps[stepId]
+                    ?: throw RuntimeException("Step id \"$stepId\" does not exist in pipeline")
+
+                step.inputs[inputId] = ConstantPipe(
+                    type,
+                    when (type) {
+                        "int" -> inputsJSON.getInt(key)
+                        "float" -> inputsJSON.getFloat(key)
+                        "boolean" -> inputsJSON.getBoolean(key)
+                        // Everything else is read as text
+                        else -> inputsJSON.getString(key)
+                    }
+                )
+            }
+
+            println(inputsJSON.toString(2))
+        }
 
         // Call validate graph
         // (Only once per final step since stored in a set. There might be some duplication lower down the tree...)
