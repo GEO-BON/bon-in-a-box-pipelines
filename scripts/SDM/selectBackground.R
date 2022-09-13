703095089f1aaf55524f668f2cf2764877c4de96
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2022-09-13 17:06:07 -0400

    #377: Used pak to perform installation (except gdalcubes which gives an error)

--- scripts/SDM/selectBackground.R
+++ scripts/SDM/selectBackground.R
@@ -1,9 +1,8 @@
 
 
 ## Install required packages
-packages <- c("terra", "rjson", "raster", "dplyr", "CoordinateCleaner", "stars")
-new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+pak::pkg_install(c("terra", "rjson", "raster", "dplyr", "CoordinateCleaner", "stars"))
+
 
 ## Load required packages
 library("terra")
