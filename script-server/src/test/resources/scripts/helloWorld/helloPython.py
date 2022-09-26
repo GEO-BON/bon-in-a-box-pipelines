2a9be27d9880f2d74ec3dbff1d4ed9771d4785f9
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2022-09-26 12:06:03 -0400

    Revert "To better see logging" THIS commit has been applied to WRONG FILE during merge! Identially named file in another folder should have been the target.
    This reverts commit aa79eed321f529dec9cb544a8227f41152a3a42d.

--- script-server/src/test/resources/scripts/helloWorld/helloPython.py
+++ script-server/src/test/resources/scripts/helloWorld/helloPython.py
@@ -7,15 +7,8 @@ data = json.load(inputFile)
 intIn = data['some_int']
 
 # Do stuff.
-print("Will start when counter reaches input value, so you see logs go by...")
-counter = 0
-for x in range(0, intIn + 1):
-  print(x, flush=True)
-  time.sleep(0.5)
-print("Go!", flush=True)
-
 if intIn == 13 :
-  print("intIn == 13, you're not lucky! This causes failure.")
+  print("some_int == 13, you're not lucky! This causes failure.")
   sys.exit(1)
 
 intIn += 1
