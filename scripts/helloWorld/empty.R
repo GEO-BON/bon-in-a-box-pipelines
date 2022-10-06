31d951c827b6bfb58ec62ca0944fbe5c400d0016
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2022-10-06 12:30:12 -0400

    #435: Inlined and reviewed github documentation (kept dev docs on a separate page for now)

---
+++ scripts/helloWorld/empty.R
@@ -0,0 +1,29 @@
+# Script location can be used to access other scripts source
+#Sys.getenv("SCRIPT_LOCATION")
+
+## Install required packages
+packages <- c("rjson")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+## Receiving output folder
+outputFolder <- commandArgs(trailingOnly=TRUE)[1]
+
+## Receiving arguments from input.json
+library("rjson")
+input <- fromJSON(file=file.path(outputFolder, "input.json"))
+
+## Script body
+<YOUR CODE HERE>
+
+## Outputing result to JSON
+output <- list(
+    # Add your outputs here "key" = "value"
+    # The output keys correspond to those described in the yml file.
+    <YOUR OUTPUTS HERE>
+    #"error" = "Some error", # halt the pipeline
+    #"warning" = "Some warning", # display a warning without halting the pipeline
+) 
+                
+jsonData <- toJSON(output, indent=2)
+write(jsonData, file.path(outputFolder,"output.json"))
