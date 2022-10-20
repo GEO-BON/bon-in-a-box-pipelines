8a24a3ce026e48bfd71411f4cb8c281a36621683
karinorman <kari@normanfamily.org>
2022-10-20 15:58:30 -0400

    Change input to an array of layer paths and weights.

--- scripts/pointSelection/combineLayers.jl
+++ scripts/pointSelection/combineLayers.jl
@@ -1,7 +1,9 @@
+using GeoArrays: read 
 using BiodiversityObservationNetworks
 using SimpleSDMLayers
 using JSON
 using Downloads
+using GeoArrays: read 
 
 # Read in input arguments and json
 outputFolder = ARGS[1]
@@ -16,30 +18,47 @@ input = JSON.parsefile(filepath)
 print(keys(input))
 
 # Assign json objects to variables
-layerpaths = input["layerpaths"]
-print(layerpaths)
-layerweights = input["layerweights"]
+layerdata = input["layerdata"]
+print(layerdata)
+
+
+#layerweights = input["layerweights"]
 targetbalance = input["targetbalance"] # this is the same as α
 
-# read in layers
-using GeoArrays: read
+# hacky way to get the number of layers based on number of weights 
+nlayers = trunc(Int, length(layerdata) / (1 + length(targetbalance)))
+layermat = permutedims(reshape(layerdata, (1 + length(targetbalance)), nlayers), (2,1))
+
+print(layermat)
+print(layermat[:, 1])
+
+
+#read in layers
 function get_simplesdmlayer(path)
     geoarray = read(path)
     SimpleSDMPredictor(geoarray.A[:,:,begin])
 end
 
-temppath = Downloads.download.(layerpaths)
+temppath = Downloads.download.(layermat[:, 1])
 layers = stack(get_simplesdmlayer.(temppath))
 
+#=
 ### Computation ###
 const numtargs = length(targetbalance)
-W = zeros(length(layerpaths), numtargs)
+W = zeros(nlayers, numtargs)
 
-for i in 1:length(layerpaths)
+for i in 1:nlayers
+    print("in loop")
     W[:,i] .= layerweights[i]
+    print("after loop")
 end 
+=#
 
-#layers = stack([geotiff(SimpleSDMPredictor, lp) for lp in layerpaths])
+# get weights with weights for a layer in columns 
+W = convert(Array{Float64}, transpose(layermat[:, 2:end]))
+print(W)
+print(typeof(W))
+print(targetbalance)
 
 priority = SimpleSDMPredictor(squish(layers, W, targetbalance))
 
@@ -55,4 +74,4 @@ print("pre_json save")
 outputDict = Dict("priority_map" => priority_path)
 open(joinpath(outputFolder, "output.json"),"w") do f
     JSON.print(f, JSON.json(outputDict)) 
-end
+end 
