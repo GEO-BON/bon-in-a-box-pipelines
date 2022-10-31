f8a1417a9e42bc7880d9a4bd335f3f6d36ce7f03
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2022-10-31 15:55:30 -0400

    merged climate velocity scripts

--- scripts/climateMetrics/climateMetrics.R
+++ scripts/climateMetrics/climateMetrics.R
@@ -39,8 +39,6 @@ outputFolder <- args[1] # Arg 1 is always the output folder
 cat(args, sep = "\n")
 
 setwd(outputFolder)
-print("output:")
-print(outputFolder)
 input <- fromJSON(file=file.path(outputFolder, "input.json"))
 print("Inputs: ")
 print(input)
@@ -55,40 +53,8 @@ source("/scripts/data/loadObservationsFunc.R")
 # Pb if buffer set to 0, transformed into empty string. Warn JM about this
 if (input$buffer_box == "...") buffer_box <- 0
 
-# Case 1: we create an extent from a set of observations
-if (input$use_obs) {
-    obs <- load_observations(species = input$species,
-        limit = input$species_limit,
-        database = "gbif",
-        year_start = input$species_year_start,
-        year_end = input$species_year_end) 
-
-# Reproject the obs to the data cube projection
-obs_pts <-
-          stacatalogue::project_coords(obs,
-                         lon = "decimal_longitude",
-                         lat = "decimal_latitude",
-                         proj_from = "+proj=longlat +datum=WGS84",
-                         proj_to = input$srs_cube)
-
-# Create the extent (data cube projection)
-bbox <- stacatalogue::points_to_bbox(obs_pts, buffer = buffer_box)
-
-
-# Case 2: we use a shapefile
-} else if (!is.null(input$shapefile_path)) {
-    obs <- NULL
-    shp <- sf::st_read(input$shapefile_path)
-    bbox <- stacatalogue::shp_to_bbox(shp,
-        proj_to = input$srs_cube)
-
-# Case 3: we use a vector
-} else if (!is.null(input$bbox)) {
-
-obs <- NULL
-bbox <- st_bbox(c(xmin = bbox[1], xmax = bbox[2], 
-            ymax = bbox[3], ymin = bbox[4]), crs = st_crs(input$srs_cube))
-    } 
+bbox <- st_bbox(c(xmin = input$bbox[1], xmax = input$bbox[2], 
+        ymax = input$bbox[3], ymin = input$bbox[4]), crs = st_crs(input$srs_cube))
 
 n_year <- as.integer(substr(input$t1, 1, 4)) - as.integer(substr(input$t0, 1, 4)) + 1 
 temporal_res <- paste0("P", n_year, "Y")
@@ -127,13 +93,15 @@ print("Future climate loaded.")
 
 
 print("Calculating metrics...")
+metric <- input$metric
+
 tif <- climate_metrics(cube_current,
                           cube_future,
-                          metric = "forward",
+                          metric,
                            t_match = input$t_match
                           )
 
-output_tif <- file.path(outputFolder, "forward_velocity.tif")
+output_tif <- file.path(outputFolder, paste0(metric, ".tif"))
 raster::writeRaster(x = tif,
                           output_tif,
                           overwrite = TRUE)
@@ -142,7 +110,7 @@ print("Metrics saved.")
 
 # Outputing result to JSON
 output <- list("output_tif" = output_tif,
-               "metric" = "forward_velocity")
+               "metric" = "local_velocity")
 
 jsonData <- toJSON(output, indent=2)
 write(jsonData, file.path(outputFolder,"output.json"))
