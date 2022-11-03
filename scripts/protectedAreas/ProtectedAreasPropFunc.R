ceb61a5c8e4fc07bdf5784c3f2c2f8f0821a608b
juanzuloaga <jzuloaga65@gmail.com>
2022-11-03 11:29:27 -0600

    Renaming files PAsProp.R to protectedAreasProp.R protectedAreas.yml to protectedAreasProp.yml
    Reviewed and adjusted parameters/labels/descriptions/types/examples in .yml file
    
    Reviewed and adjusted all lines in protectedAreasProp.R and  protectedAreasPropFunction.R

--- scripts/protectedAreas/ProtectedAreasPropFunc.R
+++ scripts/protectedAreas/ProtectedAreasPropFunc.R
@@ -2,7 +2,7 @@
 
 #' @name protected_areas
 #' @param country, character country for which to download data (e.g., "Canada) or ISO-3 codes (e.g., CAN), see https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
-#' @param bbox, a numeric vector of size 4 or 6. Coordinates of the bounding box (if use.obs is FALSE). Details in rstac::stac_search documentation.
+#' @param bbox, a numeric vector of size 4 or 6. Coordinates of the bounding box (xmin, xmax, ymax, ymin). Details in rstac::stac_search documentation. 
 #' @param crs, CRS object or a character string describing a projection and datum in the PROJ.4 format (e.g., "EPSG:6623" or "+proj=aea +lat_0=44 +lon_0=-68.5 +lat_1=60 +lat_2=46 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs" ).
 #' @param pixel_size, an integer defining the output pixel size in meters. Set at 1000m
 #' @param habitat_type,  a character vector of type of habitat c("terrestrial", "marine", "partial"), by default is c("terrestrial")."partial" is a protected area or OECM is that partially within the marine environment and partially within the terrestrial (or freshwater) environments. This value is applicable to polygons only. 
@@ -20,9 +20,9 @@ protected_areas <- function(country = "Canada",
   
   # download protected area data for your country (e.g., Canada)
   # (excluding areas represented as point localities)
-    print("Loading protected areas data set (wpda) ....")
+    print("Loading protected areas data set (wdpa) ....")
 
-    raw_pa_data <- wdpar::wdpa_fetch("Canada",
+    raw_pa_data <- wdpar::wdpa_fetch(country,
                               wait = TRUE,
                               download_dir = rappdirs::user_data_dir("wdpar") # Since data are downloaded to a temporary directory by default,
                                                                               # we will specify that the data should be downloaded to a persistent directory. This means that R won't have to re-download the same dataset every time we restart our R session,
@@ -31,7 +31,7 @@ protected_areas <- function(country = "Canada",
 
   
   # clean Country data
-    print("Cleaning protected areas data set (wpda) ....")
+    print("Cleaning protected areas data set (wdpa) ....")
     
     pa_data <- wdpar::wdpa_clean(raw_pa_data,
                           crs = crs,
@@ -70,7 +70,7 @@ protected_areas <- function(country = "Canada",
     # Select type of protected areas (e.g., "terrestrial, "marine", "partial") using bbox ('area of interest')
       print(paste0("Selecting type of habitat type = ", habitat_type, " ...."))
       
-      pa_data2 <-
+      pa_data <-
         pa_data %>%
         dplyr::filter(MARINE %in% habitat_type) %>%
         sf::st_intersection(poly)
@@ -78,8 +78,8 @@ protected_areas <- function(country = "Canada",
     # recalculate the area of each protected area
       print("Recalculating the area of each protected area ....")
       
-      pa_data2 <-
-        pa_data2 %>%
+      pa_data <-
+        pa_data %>%
         dplyr::mutate(AREA_KM2 = as.numeric(st_area(.)) * 1e-6)
 
     # Create template raster 0.001 (~1000 m)
@@ -89,13 +89,11 @@ protected_areas <- function(country = "Canada",
       terra::res(r1) <- c(0.01, 0.01)
       terra::values(r1) <- 1
       
-      
-      study_area_wgs84<- st_bbox(c(xmin = bbox[1], xmax = bbox[2], ymax = bbox[3], ymin = bbox[4]),
-                                 crs = "EPSG:4326")%>%
-        st_as_sfc()%>%
-        st_as_sf
-      
+    # Study area polygon
+      study_area_wgs84 <-  terra::vect(base::rbind(lon, lat), type = "polygons")
 
+    # Cropping raster template using study area polygon and project it.
+      print("Cropping raster template using study area polygon and project it ...")
       r_template <- terra::crop(r1, study_area_wgs84)%>%
       terra::project(res=pixel_size, mask=T,align = T, gdal=T, 
                      #"+proj=aea +lat_0=44 +lon_0=-68.5 +lat_1=60 +lat_2=46 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs",
@@ -106,11 +104,11 @@ protected_areas <- function(country = "Canada",
     # dissolve polygons into one feature (speed up calculating the fraction of raster cells covered by a polygon)
       print("Dissolving multiple polygons to one polygon feature ....")
       
-      pas_dissolved <- sf::st_union(pa_data2$geometry, by_feature = FALSE)
+      pas_dissolved <- sf::st_union(pa_data$geometry, by_feature = FALSE)
     
    
     # Calculate the fraction of raster cells covered by a polygon
-      print("Calculating the the fraction of raster cells covered by a polygon ....") 
+      print("Calculating the fraction of raster cells covered by a protected area polygon ....") 
       
       proportion_pas <- exactextractr::coverage_fraction(r_template, pas_dissolved, crop = T)
     
---
+++ scripts/protectedAreas/protectedAreasProp.R
@@ -0,0 +1,75 @@
+
+
+
+# Calculating proportion of Protected Areas (polygons) within a raster pixel (e.g., 1000m)
+
+# world database on protected areas (wdpa) source: https://www.protectedplanet.net/en
+
+
+# Packages
+
+packages <- c("sf", "wdpar", "terra", "exactextractr", "dplyr", "raster", "rjson")
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# libraries
+library("rjson")
+library("sf")
+library("wdpar")
+library("terra")
+library("exactextractr")
+library("dplyr")
+library("raster")
+options(timeout = max(60000000, getOption("timeout")))
+
+
+## Receiving args
+args <- commandArgs(trailingOnly=TRUE)
+outputFolder <- args[1] # Arg 1 is always the output folder
+
+setwd(outputFolder)
+input <- fromJSON(file=file.path(outputFolder, "input.json"))
+print("Inputs: ")
+print(input)
+
+
+# Load functions
+
+source("/scripts/protectedAreas/protectedAreasPropFunc.R")
+
+bbox <- st_bbox(c(xmin = input$bbox[1], xmax = input$bbox[2], 
+                    ymax = input$bbox[3], ymin = input$bbox[4]), crs = st_crs(input$crs))
+
+
+print("This script creates a raster layer showing the proprotion of protected areas within a given pixel size...")
+
+tif <- protected_areas(country = input$country,
+                       bbox = bbox,
+                       crs = input$crs,
+                       pixel_size = input$pixel_size,
+                       habitat_type = input$habitat_type
+)
+
+
+
+output_tif <- file.path(outputFolder, "Protected_areas_prop.tif")
+raster::writeRaster(x = tif,
+                    output_tif,
+                    overwrite = TRUE,
+                    format='COG',
+                    options=c("COMPRESS=DEFLATE")
+)
+
+
+print("PAs propotion saved.")
+
+
+
+# Outputing result to JSON
+output <- list("output_tif" = output_tif)
+
+jsonData <- toJSON(output, indent=2)
+write(jsonData, file.path(outputFolder,"output.json"))
+
+
+
