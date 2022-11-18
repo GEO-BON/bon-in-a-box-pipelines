92d1979b2d3ec761531786ecfcd9b534f6dc2d61
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2022-11-18 11:01:29 -0500

    Render scripts as a tree

--- ui/src/components/PipelineEditor/ScriptChooser.js
+++ ui/src/components/PipelineEditor/ScriptChooser.js
@@ -4,6 +4,59 @@ import {React, useState, useEffect} from 'react';
 
 const BonInABoxScriptService = require('bon_in_a_box_script_service');
 
+const onDragStart = (event, nodeType, descriptionFile) => {
+  event.dataTransfer.setData('application/reactflow', nodeType);
+  event.dataTransfer.setData('descriptionFile', descriptionFile);
+  event.dataTransfer.effectAllowed = 'move';
+};
+
+/**
+ * 
+ * @param {Array[String]} splitPathBefore Parent path, as a list of strings
+ * @param {Array[Array[String]]} splitPathLeft List of remaining paths, each being split as a list of strings
+ * @returns 
+ */
+function renderTree(splitPathBefore, splitPathLeft) {
+  // Group them by folder
+  let groupedFiles = new Map()
+  splitPathLeft.forEach(path => {
+    let first = path.shift() // first elem removed
+    let key, value
+    if (path.length > 0) {
+      key = first
+      value = path
+    } else {
+      key = ""
+      value = first
+    }
+
+    if(!groupedFiles.get(key))
+      groupedFiles.set(key, [])
+      
+    groupedFiles.get(key).push(value)
+  })
+
+  // Sort and output
+  let sortedKeys = Array.from(groupedFiles.keys()).sort()
+  return sortedKeys.map((key) => {
+    if(key === "") { // leaf
+      return groupedFiles.get(key).map(name => {
+        let descriptionFile = [...splitPathBefore, name].join('>')
+        return <div className="dndnode" onDragStart={(event) => onDragStart(event, 'io', descriptionFile)} draggable>
+          <pre>{name}</pre>
+        </div>
+      })
+    }
+    
+    // branch
+    splitPathBefore.push(key)
+    return <div key={key}>
+      <p>{key}</p>
+      <div className='inFolder'>{renderTree(splitPathBefore, groupedFiles.get(key))}</div>
+    </div>
+  })
+}
+
 export default function ScriptChooser() {
   const api = new BonInABoxScriptService.DefaultApi();
   const [scriptFiles, setScriptFiles] = useState([]);
@@ -23,26 +76,13 @@ export default function ScriptChooser() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const onDragStart = (event, nodeType, descriptionFile) => {
-    event.dataTransfer.setData('application/reactflow', nodeType);
-    event.dataTransfer.setData('descriptionFile', descriptionFile);
-    event.dataTransfer.effectAllowed = 'move';
-  };
-
   return (
     <aside className='scriptChooser'>
       <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
         Pipeline output
       </div>
       <div className="description">Available scripts:</div>
-      {scriptFiles.map((descriptionFile) => {
-        // TODO: a hierarchy?
-        return <div key={descriptionFile} className="dndnode" onDragStart={(event) => onDragStart(event, 'io', descriptionFile)} draggable>
-          <pre>
-            {descriptionFile.replace('>', '\n')}
-          </pre>
-        </div>
-      })}
+      {scriptFiles && renderTree([], scriptFiles.map(file => file.split('>')))}
     </aside>
   );
 };
