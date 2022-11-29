1d8ea8f50dc5800802d5116003b9af28e6410609
juanzuloaga <jzuloaga65@gmail.com>
2022-11-29 12:41:44 -0700

    renaming some parameters to match yml file

--- scripts/data/loadLandCover.R
+++ scripts/data/loadLandCover.R
@@ -3,12 +3,17 @@
 ## Install required packages
 
 library("devtools")
-#devtools::install_github("ReseauBiodiversiteQuebec/stac-catalogue", upgrade = "never")
-remotes::install_github("appelmar/gdalcubes_R")
+devtools::install_github("ReseauBiodiversiteQuebec/stac-catalogue", upgrade = "never")
+#devtools::install_local("C:/stac-catalogue", upgrade = "never")
+#source("C:/stac-catalogue/R/stac_functions.R")
 
 packages <- c("terra", "rjson", "raster", "dplyr", "CoordinateCleaner", "lubridate", "rgdal", "remotes")
 new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+
+library("remotes")
+
+remotes::install_github("appelmar/gdalcubes_R")
 
 
 ##devtools::install_local("loadLandCover/stac-catalogue-main.zip", 
@@ -37,37 +42,39 @@ print(input)
 
 
 # Tranform the vector to a bbox object
+
 bbox <- sf::st_bbox(c(xmin = input$bbox[1], ymin = input$bbox[2], 
-            xmax = input$bbox[3], ymax = input$bbox[4]), sf::crs = st_crs(input$proj)) 
+                  xmax = input$bbox[3], ymax = input$bbox[4]), crs = sf::st_crs(input$srs.cube))
+
 
 n_year <- as.integer(substr(input$t1, 1, 4)) - as.integer(substr(input$t0, 1, 4)) + 1 
 temporal_res <- paste0("P", n_year, "Y")
 
 if (input$stac_source == "IO") {
   lc_raster <- stacatalogue::load_prop_values(stac_path = "https://io.biodiversite-quebec.ca/stac/",
-                                collections = c("esacci-lc"), 
+                                collections = "esacci-lc", 
                               bbox = bbox,
-                               srs.cube = input$proj,
-                               limit = input$stac_limit,
+                               srs.cube = input$srs.cube,
+                               limit = 5000,
                                 t0 = input$t0,
                                 t1 = input$t1,
                                 spatial.res = input$spatial_res, # in meters
-                                prop = input$proportion,
-                                prop.res = input$proportion_res,
-                                select_values = input$lc_classes,
+                                prop = input$prop,
+                                prop.res = input$prop.res,
+                                select_values = input$select_values,
                                 temporal.res =  temporal_res)
   } else if (input$stac_source == "PC") {
   lc_raster <- stacatalogue::load_prop_values_pc(stac_path =  "https://planetarycomputer.microsoft.com/api/stac/v1/",
                                 collections = c("io-lulc-9-class"), 
                               bbox = bbox,
-                               srs.cube = input$proj,
+                               srs.cube = input$srs.cube,
                                 t0 = input$t0,
                                 t1 = input$t1,
-                                limit = input$stac_limit,
+                                limit = 5000,
                                 spatial.res = input$spatial_res, # in meters
-                                prop = input$proportion,
-                                prop.res = input$proportion_res,
-                                select_values = input$lc_classes,
+                                prop = input$prop,
+                                prop.res = input$prop.res,
+                                select_values = input$select_values,
                                 temporal.res =  temporal_res)
 }
 
@@ -76,7 +83,9 @@ if (input$stac_source == "IO") {
 output_nc_predictors <- file.path(outputFolder, "lc.tif")
 raster::writeRaster(x = lc_raster,
                           output_nc_predictors,
-                          overwrite = TRUE)
+                    format='COG',
+                    options=c("COMPRESS=DEFLATE"),
+                    overwrite = TRUE)
  
 output <- list(
   "output_lc" =  output_nc_predictors
