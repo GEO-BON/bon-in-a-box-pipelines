5745e8064157961fb84ba30996ac4c2533e5189f
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-01-09 15:50:55 -0500

    === warnings

--- ui/src/utils/ColorMapping.js
+++ ui/src/utils/ColorMapping.js
@@ -2,7 +2,7 @@
 
 function componentToHex(value) {
     var hex = value.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+    return hex.length === 1 ? "0" + hex : hex;
 }
 
 export function rgbToHex(rgb) {
