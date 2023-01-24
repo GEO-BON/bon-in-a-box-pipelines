a050f8faa82bd88a3fad953fea9992a1938f37c7
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-01-24 14:33:30 -0500

    Added an additionnal validation for duplicate ids in a pipeline file.
    This type of error was found after a json file merge.

---
+++ .github/findDuplicateIds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+RED="\033[31m"
+ENDCOLOR="\033[0m"
+
+echo "Checking for duplicate lines inside each yml files..."
+# This is a frequent error when adding an additionnal param : 
+# Copy paste the param above and forget to change the description!
+
+# Checking inside the same file only : 
+# some description duplication will occur naturally between files and it's OK.
+# ex. "Species name" description should always look the same!
+
+RESULTS=$(find . -name "*.json" -exec sh -c "echo {}; \
+grep \"\\\"id\\\": \\\"\" '{}' \
+  | sort \
+  | uniq -d \
+  | awk '{print \"${RED}\" \"[DUPLICATE] \" \$0 \"${ENDCOLOR}\"}'" \;)
+
+echo "$RESULTS"
+
+if [[ "$RESULTS" == *"[DUPLICATE]"* ]]
+  then exit 1
+fi
