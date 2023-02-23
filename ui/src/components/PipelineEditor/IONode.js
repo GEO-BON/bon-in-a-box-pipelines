b9259280a6379a73f672a20981402134ed0788b7
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-02-23 12:15:00 -0500

    Use yml instead of script name. Disambiguates when many yml files point to the same R script.

--- ui/src/components/PipelineEditor/IONode.js
+++ ui/src/components/PipelineEditor/IONode.js
@@ -46,7 +46,7 @@ export default function IONode({ id, data }) {
         })}
       </td>
       <td className='name' onMouseEnter={showScriptTooltip} onMouseLeave={hideTooltip}>
-        {metadata.script}
+        {descriptionFileLocation.split('>').map(s => <>{s}<br/></>)}
       </td>
       <td className='outputs'>
         {metadata.outputs && Object.entries(metadata.outputs).map(([outputName, desc]) => {
