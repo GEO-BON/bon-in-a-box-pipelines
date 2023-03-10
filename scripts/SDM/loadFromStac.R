b02eecfd56c0aa1052e79a37824bbbaa2e1545f8
Guillaume Larocque <glaroc@gmail.com>
2023-03-10 11:27:11 -0500

    Functioning removeCollinearity and full Maxent pipeline

--- scripts/SDM/loadFromStac.R
+++ scripts/SDM/loadFromStac.R
@@ -41,12 +41,6 @@ bbox <- sf::st_bbox(c(xmin = input$bbox[1], ymin = input$bbox[2],
             xmax = input$bbox[3], ymax = input$bbox[4]), crs = sf::st_crs(input$proj)) 
 
 
-if(is.null(input$nb_sample)) {
-  sample <- FALSE 
-} else {
-    sample <- TRUE
-}
-
 if(length(input$collections_items) == 0) {
   stop('Please specify collections_items')
 } else {
@@ -65,27 +59,8 @@ resampling = "near")
 mask=NULL
 subset_layers = input$layers
 variables = input$variables
-remove_collinear = input$remove_collinearity
-method = input$method
-method_cor_vif = input$method_cor_vif
 proj = input$proj
-bbox = bbox
-sample = sample
-nb_points = input$nb_sample
-cutoff_cor = input$cutoff_cor
-cutoff_vif = input$cutoff_vif
-export = F
-ouput_dir = getwd()
 as_list = F
-
-  
-  if (!method %in% c("none", "vif.cor", "vif.step", "pearson", "spearman", "kendall")) {
-    stop("method must be vif.cor, vif.step, pearson, spearman, or kendall")
-  }
-  
-  if (method %in% c("vif.cor", "pearson", "spearman", "kendall") && is.null(cutoff_cor)) {
-    cutoff_cor <- 0.8
-  }
   
 predictors=list()
 for (coll_it in collections_items){
@@ -111,26 +86,6 @@ for (coll_it in collections_items){
   print(names(predictors))
   nc_names <- names(predictors)
   
-  # Selection of non-collinear predictors
-  if (remove_collinear && length(nc_names) >1) {
-    if (sample) {
-      
-      env_df <- sample_spatial_obj(predictors, nb_points = nb_points)
-      
-    }
-    
-    nc_names <-detect_collinearity(env_df,
-                                   method = method ,
-                                   method_cor_vif = method_cor_vif,
-                                   cutoff_cor = cutoff_cor,
-                                   cutoff_vif = cutoff_vif,
-                                   export = export,
-                                   title_export = "Correlation plot of environmental variables.",
-                                   path = ouput_dir) 
-    
-  }                               
-  
-  
   if (as_list) {
     output <- nc_names
     
@@ -140,15 +95,9 @@ for (coll_it in collections_items){
                                              srs.cube = proj,
                                              bbox = bbox))
       output <- do.call(stacatalogue::load_cube, cube_args_nc)
-      #
       
       if(!is.null(mask)) {
-        
-      
-     output <- gdalcubes::filter_geom(cube,  sf::st_geometry(sf::st_as_sf(mask)), srs=proj)
-        
-    
-        
+        output <- gdalcubes::filter_geom(cube,  sf::st_geometry(sf::st_as_sf(mask)), srs=proj)
       }
   }
 
@@ -157,7 +106,7 @@ output_predictors <- file.path(outputFolder)
 id_rasters=list()
 for (i in 1:length(predictors)) {
   ff <- tempfile(pattern = paste0(names(predictors[i][[1]]),'_'))
-  id_rasters<-append(id_rasters,list(id=names(predictors[i][[1]]),'layer'=file.path(outputFolder, paste0(basename(ff),".tif"))))
+  id_rasters[[i]]<-list(id=names(predictors[i][[1]]),'layer'=file.path(outputFolder, paste0(basename(ff),".tif")))
   gdalcubes::write_tif(predictors[i][[1]], dir = output_predictors, prefix=basename(ff),creation_options = list("COMPRESS" = "DEFLATE"), COG=T)
 }
 
