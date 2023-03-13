e8eb2abe34badd05fa37e5098c78e54942386210
Manuel Gálvez <mgalvez@humboldt.org.co>
2023-03-13 09:11:23 -0500

    Set input polygon as parameter and output response as json

--- scripts/1_IAvH/01_pp.R
+++ scripts/1_IAvH/01_pp.R
@@ -1,7 +1,7 @@
 ### funci?n check_collections
 
 # Cargar librerias
-packages_list<-list("magrittr", "terra", "raster")
+packages_list<-list("magrittr", "terra", "raster", "jsonlite")
 invisible(lapply(packages_list, library, character.only = TRUE))
 
 # Organizar directorios
@@ -11,14 +11,13 @@ outputFolder <- args[1]
 # Cargar archivos de entrada
 input <- rjson::fromJSON(file=file.path(outputFolder, "input.json"))
 
-dir_wkt<- input$dir_wkt_polygon
+wkt_polygon<- input$wkt_polygon
 dir_colection<- input$dir_colection
 
 epsg_polygon<- input$epsg_polygon
 resolution<- input$resolution
 folder_output<- input$folder_output
 
-wkt_polygon<- readLines(dir_wkt)
 layers <- list.files(dir_colection, "\\.tif$", recursive = TRUE, full.names = TRUE)
 json_colleciton_file <- list.files(dir_colection, "\\.json$", recursive = TRUE, full.names = TRUE)
 meadata_collecion_file <- list.files(dir_colection, "\\.csv$", recursive = TRUE, full.names = TRUE)
@@ -51,7 +50,7 @@ cube_mask<- gdalcubes::filter_geom(cube, geom= wkt_polygon, srs = crs_polygon )
 
 
 # Convertir cubo a raster
-cube_stars <- stars::st_as_stars(cube_mask) %>% terra::rast()
+cube_stars <- stars::st_as_stars(cube_mask) %>% terra::rast() %>% setNames(names(cube_mask))
 
 collection_rast<- lapply(cube_stars, function(x) { if(any( is.na(summary(raster::raster(x))) )){NULL}else{x} } ) %>%
   {Filter(function(x) !is.null(x), .)} %>% {setNames(., unlist(sapply(., function(x) names(x))) )} %>% terra::rast()
@@ -76,17 +75,13 @@ saveraster<- lapply( seq(nrow(data_sum)), function(i) {
 
 } )
 
-
-
-
 # guardar tabla resumen
 setwd(folder_results)
 write.csv(data_sum,"data_summ.csv")
 
-# Imprimir resultado en logs
-print(data_sum)
+results <- jsonlite::toJSON(data_sum)
 
 ## Imprimir resultado - Formao json
-output <- list("folder_output" = folder_results)
+output <- list("results" = results)
 jsonData <- rjson::toJSON(output, indent=2)
 write(jsonData, file.path(outputFolder,"output.json"))
