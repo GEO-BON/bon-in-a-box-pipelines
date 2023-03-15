b549aa36f4774778d208892beb97094a486105d4
juanzuloaga <jzuloaga65@gmail.com>
2023-03-15 09:00:54 -0600

    Add read list of input rasters

--- scripts/binaryLayer/binaryLayerFunc.R
+++ scripts/binaryLayer/binaryLayerFunc.R
@@ -2,14 +2,11 @@
 #' @param class_lc Vector of integers, list of land cover classes to select 
 #' @param threshold_prop, float value indicating land cover proportion threshold. Values below threshold are converted to zero and above it to 1.
 #' @return a raster stack binary land cover classes 
-#' @import raster stack
+#' @import raster dplyr 
 #' @export
-
-
 
 binary_layer <- function(select_class =  c(210, 60),
                                threshold_prop = 0.8){ 
-  
 
 # Creating reclassification matrix based on threshold_prop
   m <- c(0, threshold_prop, 0, threshold_prop, 1, 1)
@@ -19,23 +16,30 @@ binary_layer <- function(select_class =  c(210, 60),
 
   # object to save the list of rasters
     lc_threshold <- list() 
+    shortnames <- list()
   
   # Loop
     for(i in 1:length(select_class)){
+     
       # Read list of input and create rasters
-      read_lc <- input$lc_classes[grep(paste(select_class[i], sep=""),input$lc_classes)]%>%raster()
-      # use layers names to keep track of lc class
-      shortnames <- input$lc_classes[grep(paste(select_class[i], sep=""),input$lc_classes)]
+        read_lc  <- raster::subset(raster::stack(input$lc_classes),
+                                 grep(select_class[i],
+                                      names(raster::stack(input$lc_classes)),
+                                      ignore.case = TRUE))
+        
       # Reclassify raster based on threshold
-      read_lc_r <- reclassify(read_lc, rclmat)
+        read_lc_r <- reclassify(read_lc, rclmat)
+     
       # Rename raster
-      names(read_lc_r) <- paste0(sub( ".*/", "", sub(".tif.*", "", shortnames)), "_bin")
+        names(read_lc_r) <- paste0(names(read_lc),  "_binary")
+     
       # Save list
-      lc_threshold[[i]] <- read_lc_r
-        }
-
+        lc_threshold[[i]] <- read_lc_r
+     
+    }
+   
   lc_threshold_s <- stack(lc_threshold) # stack rasters
-  
+ 
   return(lc_threshold_s)
 
 }
