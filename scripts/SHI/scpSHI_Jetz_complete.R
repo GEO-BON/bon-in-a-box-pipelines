408e413d73440bfb5c4a9e05cb196676bea9a434
Maria Isabel Arce <mi.arceplata@gmail.com>
2023-03-15 23:00:16 -0400

    add function to filter cube in ranges, replace image mask and simplify

--- scripts/SHI/scpSHI_Jetz_complete.R
+++ scripts/SHI/scpSHI_Jetz_complete.R
@@ -23,6 +23,7 @@ lapply(packages,require,character.only=T)
 
 path_script <- Sys.getenv("SCRIPT_LOCATION")
 source(file.path(path_script,"SHI/funGet_range_maps.R"), echo=TRUE)
+source(file.path(path_script,"SHI/funFilterCube_range.R"), echo=TRUE)
 
 input <- fromJSON(file=file.path(outputFolder, "input.json"))
 print("Inputs: ")
@@ -110,19 +111,10 @@ if(!is.na(country_code)){
   sf_area_lim2 <- sf_range_map |> st_make_valid() |> st_transform(st_crs(sf_area_lim1))
   sf_area_lim2_srs <- sf_area_lim2 |> st_transform(sf_srs)
   
-  sf_area_lim <<- st_intersection(sf_area_lim2,sf_area_lim1,dimension="polygon") |> st_make_valid() |> 
+  sf_area_lim <<- st_intersection(sf_area_lim2,sf_area_lim1,dimension="polygon") |> st_make_valid() |>
     st_collection_extract("POLYGON")
   sf_area_lim_srs <<- st_intersection(sf_area_lim2_srs,sf_area_lim1_srs,dimension="polygon") |> st_make_valid() |> 
     st_collection_extract("POLYGON")
-  
-  if(!is.null(st_crs(sf_area_lim)$units)){
-    sf_ext <<- st_bbox(sf_area_lim |> st_buffer(spat_res*10))
-  }else{
-    sf::sf_use_s2(FALSE)
-    sf_ext <<- st_bbox(sf_area_lim |> st_buffer(spat_res*0.00001*10))
-    print("--- Buffer defined for spherical geometry ---")
-  }
-  print(sf_ext)
   
   if(!is.null(st_crs(sf_area_lim_srs)$units)){
     sf_ext_srs <<- st_bbox(sf_area_lim_srs |> st_buffer(spat_res*10))
@@ -136,14 +128,6 @@ if(!is.na(country_code)){
 }else{
   sf_area_lim <<- sf_range_map
   sf_area_lim_srs <<- sf_area_lim |> st_transform(sf_srs)
-  
-  if(!is.null(st_crs(sf_area_lim)$units)){
-    sf_ext <<- st_bbox(sf_area_lim |> st_buffer(spat_res*10))
-  }else{
-    sf::sf_use_s2(FALSE)
-    sf_ext <<- st_bbox(sf_area_lim |> st_buffer(spat_res*0.00001*10))
-  }
-  print(sf_ext)
   
   if(!is.null(st_crs(sf_area_lim_srs)$units)){
     sf_ext_srs <<- st_bbox(sf_area_lim_srs |> st_buffer(spat_res*10))
@@ -173,15 +157,10 @@ df_IUCN_sheet_condition <- df_IUCN_sheet |> dplyr::mutate(
     !is.na(elevation_lower) & (as.numeric(elevation_lower) >= elev_buffer) ~ as.numeric(elevation_lower) - elev_buffer),
   max_elev= case_when(
     is.na(elevation_upper) ~ NA_real_,
-    !is.na(elevation_upper) ~ as.numeric(elevation_upper) + elev_buffer),
-  condition= case_when( #generate possible options according to availability of minimum and maximum elevation values
-    is.na(min_elev)  & is.na(max_elev)  ~ 1, # when there is no data for elevation ranges
-    !is.na(min_elev) & !is.na(max_elev) ~ 2, # when the elevation ranges are available
-    !is.na(min_elev) & is.na(max_elev)  ~ 3, # when there is just data for the maximum elevation
-    is.na(min_elev)  & !is.na(max_elev) ~ 4) # when there is just data for the minimum elevation
+    !is.na(elevation_upper) ~ as.numeric(elevation_upper) + elev_buffer)
 )
 
-with(df_IUCN_sheet_condition, if(condition == 1){ # if no elevation values are provided then the range map stays the same
+with(df_IUCN_sheet_condition, if(is.na(min_elev)  & is.na(max_elev)){ # if no elevation values are provided then the range map stays the same
   r_range_map <<- r_range_map
 }else{ # at least one elevation range exists then create cube_STRM to filter according to elevation ranges
   # STRM from Copernicus
@@ -196,20 +175,7 @@ with(df_IUCN_sheet_condition, if(condition == 1){ # if no elevation values are p
               t0 = "2021-01-01",
               t1 = "2021-12-31",
               resampling = "bilinear")
-  if(condition == 2){ # if both values exist
-    cube_STRM_range <<- cube_STRM |>
-      gdalcubes::filter_pixel(paste0("data <=", max_elev)) |>
-      gdalcubes::filter_pixel(paste0("data >= ", min_elev)) |> select_bands("data")
-  }else{ # if only one value exist
-    if(condition == 3){ # if just maximum elevation available filter by that
-      cube_STRM_range <<- cube_STRM |>
-        gdalcubes::filter_pixel(paste0("data >= ", min_elev)) |> select_bands("data")
-    }
-    if(condition == 4){ # if just minimum elevation available filter by that
-      cube_STRM_range <<- cube_STRM |>
-        gdalcubes::filter_pixel(paste0("data <=", max_elev)) |> select_bands("data")
-    }
-  }
+  cube_STRM_range <<- funFilterCube_range(cube_STRM, min = min_elev , max = max_elev) |> select_bands("data")
   #convert to raster
   r_STRM_range <<- cube_STRM_range  |> st_as_stars() |> terra::rast()
 
@@ -249,32 +215,11 @@ cube_GFW_TC <-
             t1 = "2000-12-31",
             resampling = "bilinear")
 
-forest_condition= case_when( #generate possible options according to availability of minimum and maximum forest values
-  is.na(min_forest)  & is.na(max_forest)  ~ 1, # when there is no input for tree cover percentage
-  !is.na(min_forest) & !is.na(max_forest) ~ 2, # when two percentage for tree cover needed
-  !is.na(min_forest) & is.na(max_forest)  ~ 3, # when just maximum tree cover percentage is given
-  is.na(min_forest)  & !is.na(max_forest) ~ 4) # when just minimum tree cover percentage is given
-
-if(forest_condition == 1){
+if(is.na(min_forest)  & is.na(max_forest)){
   print("--- At least one level of tree cover percentage is needed ---")
 }else{
-  if(forest_condition == 2){ # if both values are given
-    cube_GFW_TC_threshold <<- cube_GFW_TC |>
-      gdalcubes::filter_pixel(paste0("data <=", max_forest)) |>
-      gdalcubes::filter_pixel(paste0("data >= ", min_forest)) |> gdalcubes::apply_pixel("data/data")
-  }else{ # if only one value exist
-    if(forest_condition == 3){ # when just maximum tree cover percentage is given
-      cube_GFW_TC_threshold <<- cube_GFW_TC |>
-        gdalcubes::filter_pixel(paste0("data >= ", min_forest)) |> gdalcubes::apply_pixel("data/data")
-    }
-    if(forest_condition == 4){ # when just minimum tree cover percentage is given
-      cube_GFW_TC_threshold <<- cube_GFW_TC |>
-        gdalcubes::filter_pixel(paste0("data <=", max_forest)) |> gdalcubes::apply_pixel("data/data")
-    }
-  }
+  cube_GFW_TC_threshold <<- funFilterCube_range(cube_GFW_TC,min=min_forest,max=max_forest,value=FALSE)
 }
-
-
 r_GFW_TC_threshold <- cube_to_raster(cube_GFW_TC_threshold , format="terra") # convert to raster format
 
 r_range_map_rescaled <- terra::resample(r_range_map,r_GFW_TC_threshold,method="mode") #Adjust scale of range map
@@ -285,41 +230,35 @@ r_GFW_TC_threshold_mask <- r_GFW_TC_threshold |>
 print("========== Base forest layer downloaded ==========")
 
 # Download forest loss maps and create different layers for each year to remove from forest
-s_obj <- stac("https://io.biodiversite-quebec.ca/stac/")
+cube_GFW_loss <-
+  load_cube(stac_path = "https://io.biodiversite-quebec.ca/stac/",
+            limit = 1000,
+            collections = c("gfw-lossyear"),
+            bbox = sf_ext_srs,
+            srs.cube = srs_cube,
+            spatial.res = spat_res,
+            temporal.res = "P1Y",
+            t0 = "2000-01-01",
+            t1 = "2000-12-31",
+            resampling = "mode", 
+            aggregation = "first")
 
-it_obj <- s_obj |>
-  stac_search(collections = "gfw-lossyear",
-              bbox = sf_ext) |>
-  get_request()
+times <- as.numeric(substr(v_time_steps[v_time_steps>2000],start=3,stop=4))
 
-st <- gdalcubes::stac_image_collection(it_obj$features, asset_names = c("data")) # create image collection
+l_year_loss <- map(times, ~ funFilterCube_range(cube = cube_GFW_loss, max=.x, type_max=1, min=1, type_min=1, value=FALSE))
+l_r_year_loss <- map(l_year_loss, cube_to_raster, format="terra")
+s_year_loss <- rast(l_r_year_loss) |> terra::classify(rcl=cbind(NA,0))
+names(s_year_loss) <- paste0("Loss_",v_time_steps[v_time_steps>t_0])
 
-v <- cube_view(srs = srs_cube, extent = list(t0 = "2000-01-01", t1 = "2000-12-31", # create cube according to area of interest
-                                             left = sf_ext_srs['xmin'], right = sf_ext_srs['xmax'],
-                                             top = sf_ext_srs['ymax'], bottom =  sf_ext_srs['ymin']),
-               dx=spat_res, dy=spat_res, dt="P1Y",
-               resampling = "mode", aggregation = "first") # TO CHANGE to proportions
-
-times <- as.numeric(substr(v_time_steps[v_time_steps>2000],start=3,stop=4)) #get year of change by selected time step to mask map by year of change
-l_r_year_loss <- map(times, function(x) {
-  layer <- raster_cube(image_collection=st, view=v,
-                       mask=image_mask("data", values = times[times<=x],invert=T)) # to remove it to the following years
-
-  layer <- layer |>
-    stars::st_as_stars() |>
-    terra::rast()
-  return(layer)
-})
-
-#add background and mask to suitable area
-s_year_loss <- terra::classify(terra::rast(l_r_year_loss), rcl=rbind(c(NA,NA,0),c(1,Inf,1)),include.lowest=T)
-names(s_year_loss) <- paste0("Loss_",v_time_steps[v_time_steps>2000])
 s_year_loss_mask <- terra::mask(s_year_loss,r_GFW_TC_threshold_mask, maskvalues=1, inverse=TRUE)
 
 #if t_0 different of 2000 update reference forest layer "r_GFW_TC_threshold_mask" 
 if(t_0!=2000){
   r_GFW_TC_threshold_mask <- terra::classify(r_GFW_TC_threshold_mask - terra::subset(s_year_loss_mask,paste0("Loss_",t_0)),rcl=cbind(-1,0))
 }
+
+#-------------------------- figure ----------------------------------------------
+r_year_loss_mask_plot <- terra::classify(s_year_loss_mask[[length(l_r_year_loss)]],rcl=cbind(0,NA)) # turn 0 to NA
 
 cube_GFW_gain <-
   load_cube(stac_path = "https://io.biodiversite-quebec.ca/stac",
@@ -333,14 +272,10 @@ cube_GFW_gain <-
             t1 = "2000-12-31",
             resampling = "near")
 
-r_GFW_gain <- cube_to_raster(cube_GFW_gain |>
-                               stars::st_as_stars(), format="terra") # convert to raster format
+r_GFW_gain <- cube_to_raster(cube_GFW_gain , format="terra") # convert to raster format
 r_GFW_gain_mask <- terra::classify(terra::mask(r_GFW_gain ,r_range_map_rescaled),rcl=cbind(0,NA))
 
-#-------------------------- figure ----------------------------------------------
 osm <- read_osm(sf_area_lim, ext=1.1)
-
-r_year_loss_mask_plot <- terra::classify(s_year_loss_mask[[length(l_r_year_loss)]],rcl=cbind(0,NA)) # turn 0 to NA
 
 img_map_habitat_changes <- tm_shape(osm) + tm_rgb()+
   tm_shape(r_GFW_TC_threshold_mask)+tm_raster(style="cat",alpha=0.5,palette = c("#0000FF00","blue"), legend.show = FALSE)+
@@ -359,7 +294,6 @@ tmap_save(img_map_habitat_changes, img_SHI_time_period_path )
 s_HabitatArea0 <- r_GFW_TC_threshold_mask-s_year_loss_mask
 s_HabitatArea <- if(t_0!=2000)  s_HabitatArea0 else c(r_GFW_TC_threshold_mask, s_HabitatArea0) 
 rm(s_HabitatArea0)
-
 names(s_HabitatArea) <- paste0("Habitat_",v_time_steps)
 
 s_Habitat <- terra::classify(s_HabitatArea , rcl=cbind(0,NA))
