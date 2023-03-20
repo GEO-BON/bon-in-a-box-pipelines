c69de709849f3d664ce04216d51a6e34eda2168b
juanzuloaga <jzuloaga65@gmail.com>
2023-03-19 21:39:47 -0600

    New lines to save rasters with original lan dcover classes names

--- scripts/binaryLayer/binaryLayerFunc.R
+++ scripts/binaryLayer/binaryLayerFunc.R
@@ -6,42 +6,51 @@
 #' @export
 
 binary_layer <- function(lc_classes, 
-                         select_class = c(210, 60),
-                         threshold_prop = 0.8){ 
-print(paste("TEMP lc_classes", lc_classes))
+                         select_class = input$select_class,
+                         threshold_prop = input$threshold_prop){ 
+
+# let's create a raster stack for all classes and keep original classes names
+  
+  lc_classes_stack <- raster::stack(lc_classes)
+
+# object to save the list of short names
+  shortnames <- list()
+  
+  for(j in 1:length(lc_classes)){
+    
+    read_lc <- lapply(lc_classes[j], function(y){lc_classes[grep(pattern = y, x = lc_classes)]})
+    
+    shortnames[[j]] <- gsub(".*/(.*)\\..*", "\\1", read_lc)
+      
+  }
+  
+  names(lc_classes_stack) <- c(unlist(c(shortnames)))
+
 # Creating reclassification matrix based on threshold_prop
   m <- c(0, threshold_prop, 0, threshold_prop, 1, 1)
   rclmat <- matrix(m, ncol=3, byrow=T)
 
-# Looping within  land cover classes (select_class)
+  # Looping within  land cover classes (select_class)
 
   # object to save the list of rasters
     lc_threshold <- list() 
-    shortnames <- list()
-  
-  # Loop
-    for(i in 1:length(select_class)){
-        lc_stack <- raster::stack(lc_classes)
 
-        print(paste("TEMP names stack", names(lc_stack)))
-      # Read list of input and create rasters
-        read_lc  <- raster::subset(lc_stack,
-                                 grep(select_class[i],
-                                      names(lc_stack),
-                                      ignore.case = TRUE))
-        
-      # Reclassify raster based on threshold
-        read_lc_r <- reclassify(read_lc, rclmat)
+    for(i in 1:length(select_class)){
+        read_lc_sel  <- raster::subset(lc_classes_stack,
+                                       grep(select_class[i], names(lc_classes_stack), ignore.case = TRUE)
+                                       )
+        # Reclassify
+        read_lc_r <- reclassify(read_lc_sel, rclmat)
      
       # Rename raster
-        names(read_lc_r) <- paste0(names(read_lc),  "_binary")
-     
+        names(read_lc_r) <- paste0(names(read_lc_sel),  "_binary")
+    
       # Save list
         lc_threshold[[i]] <- read_lc_r
-     
+      #
     }
    
-  lc_threshold_s <- stack(lc_threshold) # stack rasters
+  lc_threshold_s <- raster::stack(lc_threshold) # stack rasters
  
   return(lc_threshold_s)
 
