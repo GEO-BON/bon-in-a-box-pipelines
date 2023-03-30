d2fd32012bf57f3bf976a2d7ca46eb3b31c7888e
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2023-03-30 17:06:38 +0000

    TG-518: Layout with elkjs

--- ui/src/components/PipelineEditor/react-flow-utils/Layout.js
+++ ui/src/components/PipelineEditor/react-flow-utils/Layout.js
@@ -1,14 +1,102 @@
 import dagre from 'dagre';
 import { getScriptDescription } from '../ScriptDescriptionStore'
 
+const ELK = require('elkjs')
+const elk = new ELK()
+
+/**
+ * 
+ * @param Nodes nodes 
+ * @param Edges edges 
+ * @param Function callback receiving the modified nodes object with the new positions
+ */
+export const layoutElements = (nodes, edges, callback) => {
+  let graph = {
+    id: 'root',
+    layoutOptions: {
+      algorithm: 'layered',
+      'layered.spacing.edgeNodeBetweenLayers': 50,
+      'spacing.nodeNode': 10
+    },
+  }
+
+  graph.children = nodes.map(node => {
+    let elkjsNode = {
+      id: node.id,
+      width: node.width,
+      height: node.height,
+    }
+
+    if (node.type === 'io') {
+      const desc = getScriptDescription(node.data.descriptionFile)
+      if(desc){
+        elkjsNode.ports = Object.keys(desc.outputs).map((output, ix) => {
+          return {
+            id: node.id + '.out.' + output,
+            side: 'EAST',
+            index: ix,
+            y: ix, // specifying y gives the top-down order of the ports
+          }
+        })
+  
+        if (desc.inputs) { // There might not always be inputs
+          Object.keys(desc.inputs).forEach((input, ix) => {
+            elkjsNode.ports.push({
+              id: node.id + '.in.' + input,
+              side: 'WEST',
+              index: ix,
+              y: ix, // specifying y gives the top-down order of the ports
+            })
+          })
+        }
+  
+        elkjsNode.properties = {
+          portConstraints: 'FIXED_ORDER',
+          'portAlignment.default': 'BEGIN'
+        }
+      } else {
+        console.error('Failed ot get description for ' + node.data.descriptionFile)
+      }
+    }
+
+    return elkjsNode
+  });
+
+  graph.edges = edges.map(edge => {
+    return {
+      id: edge.id,
+      sources: [edge.sourceHandle ? edge.source + '.out.' + edge.sourceHandle : edge.source],
+      targets: [edge.targetHandle ? edge.target + '.in.' + edge.targetHandle : edge.target]
+    }
+  });
+
+  elk.layout(graph)
+   .then(result => {
+    console.log(result)
+      // Transfer result to react-flow graph
+      nodes.forEach((reactFlowNode) => {
+        const elkjsNode = result.children.find(n => n.id === reactFlowNode.id)
+        reactFlowNode.position = {
+          x: elkjsNode.x,
+          y: elkjsNode.y
+        };
+      });
+
+      callback(nodes)
+   })
+   .catch(console.error)
+}
+
+
 /**
  * Reposition nodes using dagree graph. 
  * 
  * @param {Node[]} nodes 
  * @param {Edge[]} edges 
  * @returns (Node[], Edge[]) the provided lists modified to layout the elements
+ * @deprecated
  */
-export const layoutElements = (nodes, edges) => {
+export const layoutElementsDagre = (nodes, edges) => {
   const dagreGraph = new dagre.graphlib.Graph();
   dagreGraph.setDefaultEdgeLabel(() => ({}));
   dagreGraph.setGraph({ rankdir: 'LR', nodesep: 10, align: undefined, ranksep: 150 });
