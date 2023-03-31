5aabccb7cfe27d49006a03cf73e5c7597d8f5242
Guillaume Larocque <glaroc@gmail.com>
2023-03-31 11:45:24 -0400

    Change description and add STAC url input

--- scripts/data/loadFromStac.R
+++ scripts/data/loadFromStac.R
@@ -27,7 +27,7 @@ print(input)
 
 
 # Case 1: we create an extent from a set of observations
-bbox <- sf::st_bbox(c(xmin = input$bbox[1], ymin = input$bbox[2], 
+bbox <- sf::st_bbox(c(xmin = input$bbox[1], ymin = input$bbox[2],
             xmax = input$bbox[3], ymax = input$bbox[4]), crs = sf::st_crs(input$proj)) 
 
 
@@ -36,9 +36,7 @@ if(length(input$collections_items) == 0) {
 } else {
   collections_items <- input$collections_items
 }
-
-source = "cube"
-cube_args = list(stac_path = "http://io.biodiversite-quebec.ca/stac/",
+cube_args = list(stac_path = input$stac_url,
 limit = 5000,
 t0 = NULL,
 t1 = NULL,
@@ -46,6 +44,7 @@ spatial.res = input$spatial_res, # in meters
 temporal.res = "P1D",
 aggregation = "mean",
 resampling = "near")
+
 subset_layers = input$layers
 variables = input$variables
 proj = input$proj
@@ -67,10 +66,10 @@ for (coll_it in collections_items){
     pred <- do.call(stacatalogue::load_cube, cube_args_c)
 
      if(!is.null(mask)) {
-        pred <- gdalcubes::filter_geom(pred,  sf::st_geometry(mask))
+        pred <- gdalcubes::filter_geom(pred, sf::st_geometry(mask))
       }
       if(names(pred)=='data'){
-        pred=rename_bands(pred,data=ci[2])
+        pred=rename_bands(pred, data=ci[2])
       }
      print(pred)
 
@@ -81,14 +80,11 @@ for (coll_it in collections_items){
   
   if (as_list) {
     output <- nc_names
-    
   } else {
-    
-      cube_args_nc <- append(cube_args, list(layers = nc_names, 
+      cube_args_nc <- append(cube_args, list(layers = nc_names,
                                              srs.cube = proj,
                                              bbox = bbox))
       output <- do.call(stacatalogue::load_cube, cube_args_nc)
-      
       if(!is.null(mask)) {
         output <- gdalcubes::filter_geom(cube,  sf::st_geometry(sf::st_as_sf(mask)), srs=proj)
       }
