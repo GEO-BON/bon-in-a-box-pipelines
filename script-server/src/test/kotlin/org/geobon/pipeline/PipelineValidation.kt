ffeda42d4a59236b4d1e617cd1dc6ad2a550814b
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-06-14 10:20:40 -0400

    Make sure the error message contains the pipeline file.

--- script-server/src/test/kotlin/org/geobon/pipeline/PipelineValidation.kt
+++ script-server/src/test/kotlin/org/geobon/pipeline/PipelineValidation.kt
@@ -29,19 +29,20 @@ internal class PipelineValidation {
             if (file.isDirectory) {
                 validateAllPipelines(file)
             } else if (file.extension == "json") {
-                // Generate fake inputs
-                val fakeInputs = JSONObject()
-                val pipelineJSON = JSONObject(file.readText())
-                pipelineJSON.optJSONObject(INPUTS)?.let { inputsSpec ->
-                    inputsSpec.keySet().forEach { key ->
-                        inputsSpec.optJSONObject(key)?.let { inputSpec ->
-                            fakeInputs.put(key, inputSpec.get(INPUTS__EXAMPLE))
+                try {
+                    // Generate fake inputs
+                    val fakeInputs = JSONObject()
+                    val pipelineJSON = JSONObject(file.readText())
+                    pipelineJSON.optJSONObject(INPUTS)?.let { inputsSpec ->
+                        inputsSpec.keySet().forEach { key ->
+                            inputsSpec.optJSONObject(key)?.let { inputSpec ->
+                                fakeInputs.put(key, inputSpec.get(INPUTS__EXAMPLE))
+                            }
                         }
                     }
-                }
-                println(fakeInputs.toString(2))
+                    println(fakeInputs.toString(2))
 
-                try { // Run validation
+                    // Run validation
                     RootPipeline(file, fakeInputs.toString(2))
                 } catch (e: Exception) {
                     errorMessages += "${file.relativeTo(productionPipelinesRoot)}:\n\t${e.message}\n"
