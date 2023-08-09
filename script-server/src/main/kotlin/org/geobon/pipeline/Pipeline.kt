b6f7bddb78f81caa4cb38cdc5a11b5b22e2da0fc
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-08-09 11:05:01 -0400

    Moved init script to factory method

--- script-server/src/main/kotlin/org/geobon/pipeline/Pipeline.kt
+++ script-server/src/main/kotlin/org/geobon/pipeline/Pipeline.kt
@@ -6,138 +6,28 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import org.geobon.pipeline.RunContext.Companion.pipelineRoot
 import org.json.JSONObject
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 
 open class Pipeline private constructor(
     override val id: StepId,
-    pipelineJSON: JSONObject,
     private val descriptionFile: File,
+    private val steps:Map<String, IStep>,
     final override val inputs: MutableMap<String, Pipe>,
     final override val outputs: MutableMap<String, Output> = mutableMapOf()
 ) : IStep {
 
-    private constructor(stepId: StepId, pipelineJSON:JSONObject, descriptionFile: File, inputsJSON: String? = null) : this(
-        stepId,
-        pipelineJSON,
-        descriptionFile,
-        inputsToConstants(inputsJSON, pipelineJSON),
-    )
-
-    constructor(stepId: StepId, descriptionFile: File, inputsJSON: String? = null) : this(
-        stepId,
-        JSONObject(descriptionFile.readText()),
-        descriptionFile,
-        inputsJSON
-    )
-
-    constructor(stepId: StepId, relPath: String, inputsJSON: String? = null) : this(
-        stepId,
-        File(pipelineRoot, relPath),
-        inputsJSON
-    )
-
-    private val logger: Logger = LoggerFactory.getLogger(descriptionFile.nameWithoutExtension)
+    private val logger = LoggerFactory.getLogger(descriptionFile.nameWithoutExtension)
 
     fun getPipelineOutputs(): List<Pipe> = outputs.values.map { it }
 
-    private val steps = mutableMapOf<String, IStep>()
     private val finalSteps: Set<Step>
 
     private var job: Job? = null
 
     init {
-        val constants = mutableMapOf<String, ConstantPipe>()
-        val outputIds = mutableListOf<String>()
-
-        // Load all nodes and classify them as steps, constants or pipeline outputs
-        pipelineJSON.getJSONArray(NODES_LIST).forEach { node ->
-            if (node is JSONObject) {
-                val nodeId = node.getString(NODE__ID)
-                when (node.getString(NODE__TYPE)) {
-                    NODE__TYPE_STEP -> {
-                        val script = node.getJSONObject(NODE__DATA)
-                            .getString(NODE__DATA__FILE)
-                        val scriptFile = script.replace('>', '/')
-
-                        val stepId = StepId(script, nodeId, id)
-                        steps[nodeId] = when {
-                            scriptFile.endsWith(".json") -> Pipeline(stepId, scriptFile)
-
-                            // Instantiating kotlin "special steps".
-                            // Not done with reflection on purpose, since this could allow someone to instantiate any class,
-                            // resulting in a security breach.
-                            scriptFile == "pipeline/AssignId.yml" -> AssignId(stepId)
-                            scriptFile == "pipeline/PullLayersById.yml" -> PullLayersById(stepId)
-
-                            // Regular script steps
-                            else -> ScriptStep(scriptFile, stepId)
-                        }
-                    }
-
-                    NODE__TYPE_CONSTANT -> {
-                        val nodeData = node.getJSONObject(NODE__DATA)
-                        val type = nodeData.getString(NODE__DATA__TYPE)
-                        constants[nodeId] = createConstant(nodeId, nodeData, type, NODE__DATA__VALUE)
-                    }
-
-                    NODE__TYPE_USER_INPUT -> {
-                        val nodeData = node.getJSONObject(NODE__DATA)
-                        val type = nodeData.getString(NODE__DATA__TYPE)
-
-                        steps[nodeId] = UserInput(StepId("pipeline", nodeId, id), type)
-                    }
-
-                    NODE__TYPE_OUTPUT -> outputIds.add(nodeId)
-                    else -> logger.warn("Ignoring node type ${node.getString(NODE__TYPE)}")
-                }
-            } else {
-                logger.warn("Unexpected object type under \"nodes\": ${node.javaClass}")
-            }
-        }
-
-        // Link steps & constants by reading the edges, and populate the pipelineOutputs variable
-        pipelineJSON.getJSONArray(EDGES_LIST).forEach { edge ->
-            if(edge is JSONObject) {
-                // Find the source pipe
-                val sourceId = edge.getString(EDGE__SOURCE_ID)
-                val sourcePipe = constants[sourceId] ?: steps[sourceId]?.let { sourceStep ->
-                    val sourceOutput = edge.optString(EDGE__SOURCE_OUTPUT, Step.DEFAULT_OUT)
-                    sourceStep.outputs[sourceOutput]
-                        ?: throw Exception("Could not find output \"$sourceOutput\" in \"${sourceStep}\".\n" +
-                                "Available outputs: ${sourceStep.outputs}")
-                } ?: throw Exception("Could not find step with ID: $sourceId")
-
-                // Find the target and connect them
-                val targetId = edge.getString(EDGE__TARGET_ID)
-                if(outputIds.contains(targetId)) {
-                    if(sourcePipe is Output) {
-                        val step = steps[sourceId]
-                        val outputId =
-                            if (step is Pipeline) IOId(step.id, sourcePipe.getId())
-                            else sourcePipe.getId()
-
-                        outputs[outputId.toString()] = sourcePipe
-                    } else {
-                        throw Exception("output in json not of Output type: $targetId")
-                    }
-                } else {
-                    steps[targetId]?.let { step ->
-                        val targetInput = edge.getString(EDGE__TARGET_INPUT)
-                        step.inputs[targetInput] = step.inputs[targetInput].let {
-                            if(it == null) sourcePipe else AggregatePipe(listOf(it, sourcePipe))
-                        }
-                    } ?: logger.warn("Dangling edge: could not find target $targetId")
-                }
-
-            } else {
-                logger.warn("Unexpected object type under \"edges\": ${edge.javaClass}")
-            }
-        }
-
         finalSteps = outputs.values.mapNotNullTo(mutableSetOf()) { it.step }
-        if(finalSteps.isEmpty())
+        if (finalSteps.isEmpty())
             throw Exception("Pipeline has no designated output")
     }
 
@@ -209,7 +99,7 @@ open class Pipeline private constructor(
         }
     }
 
-    override fun validateGraph():String {
+    override fun validateGraph(): String {
         // Pipeline is valid if all its steps are
         var problems = ""
         finalSteps.forEach { problems += it.validateGraph() }
@@ -238,7 +128,11 @@ open class Pipeline private constructor(
             createRootPipeline(File(pipelineRoot, relPath), inputsJSON)
 
         fun createRootPipeline(descriptionFile: File, inputsJSON: String? = null): Pipeline {
-            return Pipeline(StepId("", ""), descriptionFile, inputsJSON).apply {
+            return createFromFile(
+                StepId("", ""),
+                descriptionFile,
+                inputsJSON
+            ).apply {
                 linkInputs()
 
                 val errors = validateGraph()
@@ -248,8 +142,114 @@ open class Pipeline private constructor(
             }
         }
 
-        private fun inputsToConstants(inputsJSON:String?, pipelineJSON: JSONObject) : MutableMap<String, Pipe> {
-            if(inputsJSON == null)
+        private fun createFromFile(stepId: StepId, relPath: String, inputsJSON: String? = null): Pipeline =
+            createFromFile(stepId, File(pipelineRoot, relPath), inputsJSON)
+
+        private fun createFromFile(stepId: StepId, descriptionFile: File, inputsJSON: String? = null): Pipeline {
+            val logger = LoggerFactory.getLogger(descriptionFile.nameWithoutExtension)
+
+            val constants = mutableMapOf<String, ConstantPipe>()
+            val outputIds = mutableListOf<String>()
+            val steps:MutableMap<String, IStep> = mutableMapOf()
+            val outputs: MutableMap<String, Output> = mutableMapOf()
+
+            // Load all nodes and classify them as steps, constants or pipeline outputs
+            val pipelineJSON = JSONObject(descriptionFile.readText())
+            pipelineJSON.getJSONArray(NODES_LIST).forEach { node ->
+                if (node is JSONObject) {
+                    val nodeId = node.getString(NODE__ID)
+                    when (node.getString(NODE__TYPE)) {
+                        NODE__TYPE_STEP -> {
+                            val script = node.getJSONObject(NODE__DATA)
+                                .getString(NODE__DATA__FILE)
+                            val scriptFile = script.replace('>', '/')
+
+                            val innerStepId = StepId(script, nodeId, stepId)
+                            steps[nodeId] = when {
+                                scriptFile.endsWith(".json") -> createFromFile(innerStepId, scriptFile)
+
+                                // Instantiating kotlin "special steps".
+                                // Not done with reflection on purpose, since this could allow someone to instantiate any class,
+                                // resulting in a security breach.
+                                scriptFile == "pipeline/AssignId.yml" -> AssignId(innerStepId)
+                                scriptFile == "pipeline/PullLayersById.yml" -> PullLayersById(innerStepId)
+
+                                // Regular script steps
+                                else -> ScriptStep(scriptFile, innerStepId)
+                            }
+                        }
+
+                        NODE__TYPE_CONSTANT -> {
+                            val nodeData = node.getJSONObject(NODE__DATA)
+                            val type = nodeData.getString(NODE__DATA__TYPE)
+                            constants[nodeId] = createConstant(nodeId, nodeData, type, NODE__DATA__VALUE)
+                        }
+
+                        NODE__TYPE_USER_INPUT -> {
+                            val nodeData = node.getJSONObject(NODE__DATA)
+                            val type = nodeData.getString(NODE__DATA__TYPE)
+
+                            steps[nodeId] = UserInput(StepId("pipeline", nodeId, stepId), type)
+                        }
+
+                        NODE__TYPE_OUTPUT -> outputIds.add(nodeId)
+                        else -> logger.warn("Ignoring node type ${node.getString(NODE__TYPE)}")
+                    }
+                } else {
+                    logger.warn("Unexpected object type under \"nodes\": ${node.javaClass}")
+                }
+            }
+
+            // Link steps & constants by reading the edges, and populate the pipelineOutputs variable
+            pipelineJSON.getJSONArray(EDGES_LIST).forEach { edge ->
+                if (edge is JSONObject) {
+                    // Find the source pipe
+                    val sourceId = edge.getString(EDGE__SOURCE_ID)
+                    val sourcePipe = constants[sourceId] ?: steps[sourceId]?.let { sourceStep ->
+                        val sourceOutput = edge.optString(EDGE__SOURCE_OUTPUT, Step.DEFAULT_OUT)
+                        sourceStep.outputs[sourceOutput]
+                            ?: throw Exception("Could not find output \"$sourceOutput\" in \"${sourceStep}\".\n" +
+                                    "Available outputs: ${sourceStep.outputs}")
+                    } ?: throw Exception("Could not find step with ID: $sourceId")
+
+                    // Find the target and connect them
+                    val targetId = edge.getString(EDGE__TARGET_ID)
+                    if (outputIds.contains(targetId)) {
+                        if (sourcePipe is Output) {
+                            val step = steps[sourceId]
+                            val outputId =
+                                if (step is Pipeline) IOId(step.id, sourcePipe.getId())
+                                else sourcePipe.getId()
+
+                            outputs[outputId.toString()] = sourcePipe
+                        } else {
+                            throw Exception("output in json not of Output type: $targetId")
+                        }
+                    } else {
+                        steps[targetId]?.let { step ->
+                            val targetInput = edge.getString(EDGE__TARGET_INPUT)
+                            step.inputs[targetInput] = step.inputs[targetInput].let {
+                                if (it == null) sourcePipe else AggregatePipe(listOf(it, sourcePipe))
+                            }
+                        } ?: logger.warn("Dangling edge: could not find target $targetId")
+                    }
+
+                } else {
+                    logger.warn("Unexpected object type under \"edges\": ${edge.javaClass}")
+                }
+            }
+
+            return Pipeline(
+                stepId,
+                descriptionFile,
+                steps,
+                inputsToConstants(inputsJSON, pipelineJSON),
+                outputs
+            )
+        }
+
+        private fun inputsToConstants(inputsJSON: String?, pipelineJSON: JSONObject): MutableMap<String, Pipe> {
+            if (inputsJSON == null)
                 return mutableMapOf()
 
             val inputsParsed = JSONObject(inputsJSON)
@@ -267,7 +267,12 @@ open class Pipeline private constructor(
             return constants
         }
 
-        private fun createConstant(idForUser: String, obj: JSONObject, type:String, valueProperty:String): ConstantPipe {
+        private fun createConstant(
+            idForUser: String,
+            obj: JSONObject,
+            type: String,
+            valueProperty: String
+        ): ConstantPipe {
 
             return if (type.endsWith("[]")) {
                 val jsonArray = try {
