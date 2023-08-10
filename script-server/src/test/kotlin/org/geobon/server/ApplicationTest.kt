4ff8390ed311a9bbfeecf212089f8cb7d1dcf47f
Jean-Michel <jean-michel.lord@mcgill.ca>
2023-08-10 11:49:26 -0400

    Always using test paths for integrated testing There was a problem in test isolation that made the test fail.

--- script-server/src/test/kotlin/org/geobon/server/ApplicationTest.kt
+++ script-server/src/test/kotlin/org/geobon/server/ApplicationTest.kt
@@ -30,41 +30,39 @@ class ApplicationTest {
 
     @Test
     fun testPipelineRun() = testApplication {
-        withProductionPaths {
-            application {
-                configureRouting()
-            }
+        application {
+            configureRouting()
+        }
 
-            client.get("/pipeline/list").apply {
-                assertEquals(HttpStatusCode.OK, status)
-                val result = bodyAsText()
-                val jsonResult = JSONArray(result)
-                println("Available scripts: $result")
-                assertTrue(jsonResult.length() > 0)
-                assertContains(jsonResult, "helloWorld.json")
-            }
+        client.get("/pipeline/list").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val result = bodyAsText()
+            val jsonResult = JSONArray(result)
+            println("Available scripts: $result")
+            assertTrue(jsonResult.length() > 0)
+            assertContains(jsonResult, "helloWorld.json")
+        }
 
-            var id:String
-            client.post("/pipeline/helloWorld.json/run") {
-                setBody("{\"helloWorld>helloPython.yml@0|some_int\":1}")
-            }.apply {
-                assertEquals(HttpStatusCode.OK, status)
-                id = bodyAsText()
-            }
+        var id:String
+        client.post("/pipeline/helloWorld.json/run") {
+            setBody("{\"helloWorld>helloPython.yml@0|some_int\":1}")
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+            id = bodyAsText()
+        }
 
 
-            client.get("/pipeline/$id/outputs").apply {
-                val result = JSONObject(bodyAsText())
-                println(result)
+        client.get("/pipeline/$id/outputs").apply {
+            val result = JSONObject(bodyAsText())
+            println(result)
 
-                val folder = File(
-                    outputRoot,
-                    result.getString(result.keys().next()))
-                assertTrue(folder.isDirectory)
+            val folder = File(
+                outputRoot,
+                result.getString(result.keys().next()))
+            assertTrue(folder.isDirectory)
 
-                val files = folder.listFiles()
-                assertTrue(files!!.size >= 3, "Expected input, output and log files to be there.\nFound ${files.toList()}")
-            }
+            val files = folder.listFiles()
+            assertTrue(files!!.size >= 3, "Expected input, output and log files to be there.\nFound ${files.toList()}")
         }
     }
 
@@ -99,14 +97,12 @@ class ApplicationTest {
 
     @Test
     fun testIgnoreTrailingSlash() = testApplication {
-        withProductionPaths {
-            client.get("/pipeline/list").apply {
-                assertEquals(HttpStatusCode.OK, status)
-            }
+        client.get("/pipeline/list").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
 
-            client.get("/pipeline/list/").apply {
-                assertEquals(HttpStatusCode.OK, status)
-            }
+        client.get("/pipeline/list/").apply {
+            assertEquals(HttpStatusCode.OK, status)
         }
     }
 }
