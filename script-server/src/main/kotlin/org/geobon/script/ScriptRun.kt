6343f923b9e2cf266c467ce981d26d02a04894bd
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-08-16 14:32:25 -0400

    Remove TODO: At the moment where the waiting starts, we have no guarantee that the job has already started.  Therefore, we cannot use the join.   We would need an ugly combination of polling until task to join with appears.

--- script-server/src/main/kotlin/org/geobon/script/ScriptRun.kt
+++ script-server/src/main/kotlin/org/geobon/script/ScriptRun.kt
@@ -50,10 +50,9 @@ class ScriptRun( // Constructor used in single script run
 
     /**
      * It is possible that two scripts are executed with the same parameters at the same time.
-     * If so, we wait for the other one to complete and then use it's result as cache.
+     * If so, we wait for the other one to complete and then use its result as cache.
      */
     suspend fun waitForResults() {
-        // TODO: Could use join() on a job, if we had a job to join with...
         logger.debug("Waiting for run completion... {}", context.outputFolder)
         while(!this::results.isInitialized) {
             delay(100L)
