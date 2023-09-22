fcb9f752e17bb013a78880131587183db2766d3a
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-09-22 16:34:59 -0400

    TG-344: Simple monaco integration

--- ui/src/components/PipelineEditor/MetadataPane.js
+++ ui/src/components/PipelineEditor/MetadataPane.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Editor from '@monaco-editor/react';
+
+const yaml = require('js-yaml');
+
+// TODO: See https://github.com/suren-atoyan/monaco-react for configuration
 
 /**
  * @returns rendered view of the pipeline inputs and outputs
@@ -6,7 +11,7 @@ import React, { useState } from "react";
 export const MetadataPane = ({
   metadata
 }) => {
-  const [collapsedPane, setCollapsedPane] = useState(false);
+  const [collapsedPane, setCollapsedPane] = useState(true);
   return (
     <div className={`rightPane metadataPane ${collapsedPane ? "paneCollapsed" : "paneOpen"}`}>
       <div className="collapseTab" onClick={() => setCollapsedPane(!collapsedPane)}>
@@ -22,8 +27,7 @@ export const MetadataPane = ({
       </div>
       <div className="rightPaneInner">
         <h3>Metadata</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at egestas dui. Sed accumsan felis eget mi congue, id suscipit elit dictum. Proin lorem nunc, scelerisque at diam ut, vehicula aliquam magna. Duis non nunc elementum, bibendum enim a, imperdiet nulla. Duis odio nisi, porttitor non imperdiet at, fringilla ac nisi. Sed dictum neque lectus, ut suscipit tortor accumsan a. Mauris vehicula semper eleifend. Aenean urna dui, vulputate quis viverra sit amet, cursus placerat neque.</p>
-        <p>Quisque vestibulum luctus urna vitae luctus. Nunc eget mauris metus. Sed ipsum erat, aliquam ac viverra ut, convallis id ante. Nam aliquet et sem ut volutpat. Aliquam at ullamcorper tortor, quis eleifend nunc. Suspendisse efficitur nisl in erat fermentum, et congue arcu fermentum. In ut diam sed nulla tincidunt scelerisque. Fusce congue aliquet nibh, in viverra ipsum auctor sit amet. Proin auctor sapien dolor, ut dignissim leo auctor a. </p>
+        <Editor defaultLanguage="yaml" defaultValue={yaml.dump(metadata)} />;
       </div>
     </div>
   );
