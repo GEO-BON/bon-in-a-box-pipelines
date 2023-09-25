23aab77938a8e60ddd41a036109e4f3a4fc32719
Jean-Michel <jean-michel.lord@mcgill.ca>
2023-09-25 16:52:07 -0400

    Updating a string state.

--- ui/src/components/PipelineEditor/MetadataPane.js
+++ ui/src/components/PipelineEditor/MetadataPane.js
@@ -5,13 +5,28 @@ const yaml = require('js-yaml');
 
 // TODO: See https://github.com/suren-atoyan/monaco-react for configuration
 
+const emptyMetadata = `
+name: # short name, such as My Script
+description: # Targetted to those who will interpret pipeline results and edit pipelines.
+author: # 1 to many
+  - name: # Full name
+    identifier: # Optional, full URL of a unique digital identifier such as an ORCID
+license: # Optional. If unspecified, the project's MIT license will apply.
+external_link: # Optional, link to a separate project, github repo, etc.
+`
+
 /**
  * @returns rendered view of the pipeline inputs and outputs
  */
 export const MetadataPane = ({
-  metadata
+  metadata, setMetadata
 }) => {
   const [collapsedPane, setCollapsedPane] = useState(true);
+
+  function handleEditorChange(value, event) {
+    setMetadata(value)
+  }
+
   return (
     <div className={`rightPane metadataPane ${collapsedPane ? "paneCollapsed" : "paneOpen"}`}>
       <div className="collapseTab" onClick={() => setCollapsedPane(!collapsedPane)}>
@@ -26,7 +41,8 @@ export const MetadataPane = ({
       <div className="rightPaneInner">
         <Editor
           defaultLanguage="yaml"
-          defaultValue={yaml.dump(metadata)}
+          defaultValue={metadata === "" ? emptyMetadata : metadata}
+          onChange={handleEditorChange}
           options={{
             lineNumbers:"off",
             minimap: { enabled: false },
