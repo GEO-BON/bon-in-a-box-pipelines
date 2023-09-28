c66e356c7a3331cc8c2d9c74decc03053fb78499
Jean-Michel <jean-michel.lord@mcgill.ca>
2023-09-28 13:08:43 -0400

    update cache version due to change in the way inputs are hashed.

--- script-server/src/main/kotlin/org/geobon/server/plugins/Cache.kt
+++ script-server/src/main/kotlin/org/geobon/server/plugins/Cache.kt
@@ -7,7 +7,7 @@ import java.io.File
 import java.io.IOException
 import kotlin.io.path.moveTo
 
-const val CACHE_VERSION = "2.0"
+const val CACHE_VERSION = "2.1"
 val CACHE_VERSION_FILE = File(outputRoot, "cacheVersion.txt")
 
 fun checkCacheVersion() {
