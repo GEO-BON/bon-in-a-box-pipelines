c89134316c29571d67c36713c9cc3268c85d805c
michael <mdcatchen@gmail.com>
2023-10-07 11:47:02 -0400

    :tada: combined BRT and priority map pipeline

--- scripts/Block3/weightLayers.jl
+++ scripts/Block3/weightLayers.jl
@@ -3,7 +3,7 @@ using CSV
 using DataFrames
 using SpeciesDistributionToolkit
 
-
+include("shared.jl")
 
 function write_outputs(runtime_dir, priority)
     outpath = joinpath(runtime_dir, "priority.tif")
@@ -15,6 +15,15 @@ function write_outputs(runtime_dir, priority)
     end 
 end
 
+function get_shared_indices(layers)
+    land_idxs = []
+    for l in layers
+        push!(land_idxs, findall(!isnothing, l.grid))
+    end
+
+    ∩(land_idxs...)
+end
+
 function main()
     runtime_dir = ARGS[1]   
     inputs = read_inputs_dict(runtime_dir)
@@ -24,13 +33,30 @@ function main()
     uncert_path = inputs["uncertainty"]
     uniqueness_path = inputs["climate_uniqueness"]
     velocity_path = inputs["climate_velocity"]
-    access_path = inputs["accessability"]
+    
+    access_path = inputs["accessibility"]
     
     layer_paths = [uncert_path, uniqueness_path, velocity_path, access_path]
 
     layers = [rescale(SimpleSDMPredictor(joinpath(runtime_dir, layer_path)), (0,1)) for layer_path in layer_paths]
+    shared_idxs = get_shared_indices(layers)
 
-    priority = sum([β[i] .* layers[i].grid for i in eachindex(β)])
+    access = similar(layers[begin])
+    access.grid .= nothing
+    # note accessability gets inverted becaused smaller = better 
+    access.grid[shared_idxs] .= 1 .- layers[end].grid[shared_idxs]
+    access = rescale(access, (0,1))
+    layers[4] = access 
+
+    priority = similar(layers[begin])
+    priority.grid .= nothing
+    priority.grid[shared_idxs] .= 0.
+
+    for (i,l) in enumerate(layers)
+        priority.grid[shared_idxs] .+= β[i] .* l.grid[shared_idxs]
+    end 
+   
+    priority = rescale(priority, (0,1))
 
     write_outputs(runtime_dir, priority)    
 end 
