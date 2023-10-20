8012db3bd68947aeb786c3d6709be7e007cfb441
michael d. catchen <mdcatchen@gmail.com>
2023-10-20 14:10:02 -0400

    Script specific Project.toml

--- runners/instantiate.jl
+++ runners/instantiate.jl
@@ -1,3 +1,18 @@
 using Pkg
-Pkg.activate(@__DIR__); 
-Pkg.instantiate();
+
+# A dictionary with keys that are the path to a script directory, 
+# and the values are an array of PackageSpecs that fix each required 
+# package's name and version.  
+script_dict = Dict(
+    # helloJulia.jl
+    joinpath("scripts", "helloWorld", "helloJulia") =>
+        [PackageSpec(; name="JSON", version=v"0.21.4")],
+)
+
+for (dir, packages) in script_dict
+    Pkg.activate(joinpath("/root/",dir)); 
+    Pkg.add.(packages)
+    Pkg.instantiate();
+end
+
+
