dbec90c9fb087c76d8ab42c24e02c7861f896e02
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-07 16:22:22 -0500

    remove server stuff (now in bon-in-a-box-pipeline-engine repo)

--- script-server/hotswap.sh
+++
@@ -1,7 +0,0 @@
-docker exec -it biab-script-server sh -c "cd /home/gradle/project/ && gradle assemble"
-
-
-# optional
-# Since Auto-reload detects changes in output files, we can enable automatic project rebuilding.
-# To do this, execute the following command in a repository's root directory:
-#./gradlew -t :autoreload-engine-main:build
