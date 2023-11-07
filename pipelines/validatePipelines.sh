82e5363281d0b61c4e00aabaa3c49e27d4f462c8
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-07 13:54:48 -0500

    message for windows users

--- pipelines/validatePipelines.sh
+++ pipelines/validatePipelines.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Windows users: using powershell, type "bash" command to enter bash, then run this script.
+# See https://stackoverflow.com/a/44359679/3519951
+
 ../.server/.github/findDuplicateIds.sh
