1d6a8fce91bd79d8f56c8a62bfe93dd3fcf32cc5
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-07 16:22:22 -0500

    remove server stuff (now in bon-in-a-box-pipeline-engine repo)

--- script-server/src/main/kotlin/org/geobon/pipeline/IStep.kt
+++
@@ -1,8 +0,0 @@
-package org.geobon.pipeline
-
-interface IStep : PipelinePart {
-    val id: StepId
-    val inputs: MutableMap<String, Pipe>
-    val outputs: Map<String, Output>
-    suspend fun execute()
-}
--- ui/src/utils/isEmptyObject.js
+++
@@ -1,5 +0,0 @@
-// https://stackoverflow.com/a/34491966/3519951
-export function isEmptyObject(obj) {
-  for (var _ in obj) { return false; }
-  return true;
-}
