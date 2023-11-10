14953cfbc8703513b093b816270a4f713f6f0bd9
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-10 16:09:45 -0500

    WIP run validation

--- pipelines/validatePipelines.sh
+++ pipelines/validatePipelines.sh
@@ -4,3 +4,8 @@
 # See https://stackoverflow.com/a/44359679/3519951
 
 ../.server/.github/findDuplicateIds.sh
+
+# Server not running TODO: not working...
+#docker run geobon/bon-in-a-box:script-server java -cp biab-script-server.jar org.geobon.pipeline.Validator
+# When server running:
+docker exec -it biab-script-server java -cp biab-script-server.jar org.geobon.pipeline.Validator
