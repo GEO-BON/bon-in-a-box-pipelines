f1a73c106e01663faa6164e75bb4ede5acc5d225
Jean-Michel <jean-michel.lord@mcgill.ca>
2023-11-20 14:54:09 -0500

    referring to main branch (will fail until main branch merged)

--- validate.sh
+++ validate.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Windows users: using powershell, type "bash" command to enter bash, then run this script.
-# See https://stackoverflow.com/a/44359679/3519951
-
 .server/prod-server.sh validate
