a7c475767a14b98a8c1527c1fc6bb0ee8e2058ec
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2023-11-28 16:35:48 -0500

    Changed to work with the updated server script.

--- scripts/data/pyLoadObservations/pyLoadObservations.sh
+++ scripts/data/pyLoadObservations/pyLoadObservations.sh
@@ -1,4 +1,8 @@
 #!bin/sh
+
+# Windows path conversion from C:\somedir\somefile to /c/somedir/somefile
+export HOST_PATH=$(echo $PIPELINE_REPO_PATH | sed -e 's/^C:/\/c/' -e 's/\\/\//g')
+echo "Run in $HOST_PATH"
+
 docker-compose -f /scripts/data/pyLoadObservations/docker/docker-compose.yaml build
 docker-compose -f /scripts/data/pyLoadObservations/docker/docker-compose.yaml run pc-dock python -u /home/jovyan/scripts/data/pyLoadObservations/getocc/get_occ.py ${1}
-#test254
