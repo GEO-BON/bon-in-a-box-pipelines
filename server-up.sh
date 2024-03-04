7a39a748f54beec17eee7192def434640bdaa2c6
Jean-Michel <jean-michel.lord@mcgill.ca>
2024-03-04 14:30:58 -0500

    Added assertion to quickly fail if something goes wrong. I would move on with the process, making the real error unclear.

--- server-up.sh
+++ server-up.sh
@@ -6,9 +6,17 @@
 # Optional arg 1: branch name of server repo, default "main"
 branch=${1:-"main"}
 shift
-# Additionnal optional args will be appended to the docker compose up command. 
+# Additionnal optional args will be appended to the docker compose up command.
 # Typical use is to give a specific service name to (re)start only that one.
 options=$@
+
+function assertSuccess {
+    RED="\033[31m"
+    ENDCOLOR="\033[0m"
+    if [[ $? -ne 0 ]] ; then
+        echo -e "${RED}FAILED${ENDCOLOR}" ; exit 1
+    fi
+}
 
 echo "Updating server init script..."
 if cd .server; then
@@ -23,13 +31,18 @@ if cd .server; then
         ls -a | grep -Ev "^(\.git|\.|\.\.)$" | xargs rm -r
     fi
 
-    git fetch --no-tag --depth 1 origin $branch 
-else 
+    git fetch --no-tag --depth 1 origin $branch
+    assertSuccess
+else
     git clone -n git@github.com:GEO-BON/bon-in-a-box-pipeline-engine.git --branch $branch --single-branch .server --depth 1
-    cd .server;
+    assertSuccess
+    cd .server
+    assertSuccess
 fi
 
 echo "Using branch $branch."
 git checkout origin/$branch -- prod-server.sh
+assertSuccess
+
 ./prod-server.sh checkout $branch
 ./prod-server.sh up $options
