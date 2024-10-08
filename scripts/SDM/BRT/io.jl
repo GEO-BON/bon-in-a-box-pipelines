3fe18fbd62994b52a37240159b5c26f9527ed348
michael d. catchen <mdcatchen@gmail.com>
2024-10-08 15:11:33 -0400

    io fix

--- scripts/SDM/BRT/io.jl
+++ scripts/SDM/BRT/io.jl
@@ -1,3 +1,7 @@
+using SpeciesDistributionToolkit
+using SpeciesDistributionToolkit.SimpleSDMLayers
+const _ARCHGDAL = SpeciesDistributionToolkit.SimpleSDMLayers.ArchGDAL
+
 function read_inputs_dict(runtime_dir)
     filepath = joinpath(runtime_dir, "input.json")
     output_dir = joinpath(runtime_dir, "data/")
@@ -5,13 +9,55 @@ function read_inputs_dict(runtime_dir)
     return JSON.parsefile(filepath)
 end
 
+function _prepare_layer_for_burnin(layer::SDMLayer{T}, nodata::T) where {T<:Number}
+    array = copy(layer.grid)
+    array[findall(.!layer.indices)] .= nodata
+    array_t = reverse(permutedims(array, [2, 1]); dims=2)
+    return array_t
+end
+
+function _get_geotransform(layer)
+    gt = zeros(Float64, 6)
+    gt[1] = layer.x[1]
+    gt[2] = 2stride(layer, 1)
+    gt[3] = 0.0
+    gt[4] = layer.y[2]
+    gt[5] = 0.0
+    gt[6] = -2stride(layer, 2)
+    return gt
+end
+
+function _write_tif(layer::SDMLayer{T}, filename) where {T}
+    array_t = _prepare_layer_for_burnin(layer, typemin(T))
+    width, height = size(array_t)
+    gt = _get_geotransform(layer)
+
+    _ARCHGDAL.create(
+        filename,
+        width=width,
+        height=height,
+        nbands=1,
+        dtype=Float64,
+        options=["COMPRESS=COG"],
+        driver=_ARCHGDAL.getdriver("GTiff")
+    ) do dataset
+        band = _ARCHGDAL.getband(dataset, 1)
+        _ARCHGDAL.setnodatavalue!(band, typemin(T))
+        _ARCHGDAL.setgeotransform!(dataset, gt)
+        _ARCHGDAL.setproj!(dataset, layer.crs)
+        _ARCHGDAL.write!(band, array_t)
+    end
+end
 
 function write_outputs(
     runtime_dir,
     fit_stats,
     uncertainty,
     rangemap,
-    predicted_sdm
+    predicted_sdm,
+    pseudoabsences,
+    tuning,
+    corners
 )
 
     sdm_path = joinpath(runtime_dir, "sdm.tif")
@@ -30,10 +76,13 @@ function write_outputs(
 
     open(output_json_path, "w") do f
         write(f, JSON.json(Dict(
-            :fit_stats => fit_stats,
+            :fit_stats => fit_stats_path,
             :sdm_uncertainty => uncert_path,
-            :range => sdm_path,
-            :predicted_sdm => sdm_path
+            :range => range_path,
+            :predicted_sdm => sdm_path,
+            :pseudoabsences => pa_path,
+            :env_corners => corners_path,
+            :tuning => tuning_path
         )))
     end
 
