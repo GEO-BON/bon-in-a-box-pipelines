ce8a6471e68e4ed974720b5088aa837ded996999
Jean-Michel Lord (server) <jean-michel.lord@mcgill.ca>
2024-11-06 19:10:14 +0000

    stacatalogue no longer used here

--- scripts/filtering/cleanCoordinates.R
+++ scripts/filtering/cleanCoordinates.R
@@ -7,7 +7,6 @@ new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library("devtools")
-if (!"stacatalogue" %in% installed.packages()[,"Package"]) devtools::install_github("ReseauBiodiversiteQuebec/stac-catalogue")
 if (!"gdalcubes" %in% installed.packages()[,"Package"]) devtools::install_github("appelmar/gdalcubes_R")
 
 ## Load required packages
@@ -27,15 +26,15 @@ input <- fromJSON(file=file.path(outputFolder, "input.json"))
 print("Inputs: ")
 print(input)
 
-presence <- read.table(file = input$presence, sep = '\t', header = TRUE) 
+presence <- read.table(file = input$presence, sep = '\t', header = TRUE)
 
 # Import of predictors
 predictors <- terra::rast(unlist(input$predictors))
 proj <- terra::crs(predictors)
 
 # Projecting observations into predictors projection
-presence <- create_projection(presence, lon = "decimal_longitude", lat = "decimal_latitude", 
-proj_from = "+proj=longlat +datum=WGS84", proj_to = proj, new_lon = "lon", new_lat = "lat") 
+presence <- create_projection(presence, lon = "decimal_longitude", lat = "decimal_latitude",
+proj_from = "+proj=longlat +datum=WGS84", proj_to = proj, new_lon = "lon", new_lat = "lat")
 
 # Cleaning the data
   clean_presence <- clean_coordinates(
@@ -58,12 +57,12 @@ proj_from = "+proj=longlat +datum=WGS84", proj_to = proj, new_lon = "lon", new_l
 
 clean_presence.output <- file.path(outputFolder, "clean_presence.tsv")
 
-write.table(clean_presence, clean_presence.output, 
+write.table(clean_presence, clean_presence.output,
              append = F, row.names = F, col.names = T, sep = "\t")
 
 
   output <- list("n_clean" = nrow(clean_presence),
                   "clean_presence" = clean_presence.output
-                  ) 
+                  )
 jsonData <- toJSON(output, indent=2)
 write(jsonData, file.path(outputFolder,"output.json"))
