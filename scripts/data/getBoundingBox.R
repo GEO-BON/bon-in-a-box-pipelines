837f429036a728ab8e9e48ebda4a2284fe4000b0
JoryGriffith <jory.griffith@gmail.com>
2024-11-13 16:58:38 -0500

    cropped raster by country for viewer, added link to valid country names for bounding box

--- scripts/data/getBoundingBox.R
+++ scripts/data/getBoundingBox.R
@@ -4,7 +4,8 @@ library("rjson")
 library("sf")
 
 input <- fromJSON(file=file.path(outputFolder, "input.json"))
-  
+
+output<- tryCatch({
 # define study area
 if (is.null(input$studyarea_file)){ # if there is no study area file input
   if (is.null(input$state)){ # if there is only a country input (no state) 
@@ -19,7 +20,7 @@ if (is.null(input$studyarea_file)){ # if there is no study area file input
 study_area_polygon<- sf::st_read(study_area)  # load study area as sf object
 print(st_crs(study_area_polygon))
 if(nrow(study_area_polygon)==0){
-  stop("Study area polygon does not exist. Check spelling of country and state names. Check if region contains protected areas")
+  stop("Study area polygon does not exist. Check spelling of country and state names.")
 }  # stop if object is empty
 
 # Save study area and protected area data
@@ -34,8 +35,7 @@ bbox <- st_transform(study_area_polygon, st_crs(4326))
 bbox<-unname(st_bbox(study_area_polygon))
 
 output <- list("bbox" = bbox, "study_area_polygon" = study_area_polygon_path)
-print(output)
-
+}, error = function(e) { list(error = conditionMessage(e)) })
 ### return outpu
 jsonData <- toJSON(output, indent=2)
 write(jsonData, file.path(outputFolder,"output.json"))
