4a07ff4246fb1fd7a1433998de1c0750e72f6d59
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2024-12-11 16:56:08 -0500

    Partially edited with biab_output function

--- scripts/helloWorld/helloR.R
+++ scripts/helloWorld/helloR.R
@@ -17,16 +17,21 @@ example_jpg = file.path(outputFolder, "example.jpg")
 if(!file.exists(example_jpg)) {
     download.file("https://geobon.org/wp-content/uploads/2018/01/default-image.png", example_jpg, "auto")
 }
+biab_outputs("some_picture", example_jpg)
 
 example_tiff = file.path(outputFolder, "utmsmall.tif")
 if(!file.exists(example_tiff)) {
     download.file("https://github.com/yeesian/ArchGDALDatasets/raw/master/data/utmsmall.tif", example_tiff, "auto")
 }
+biab_outputs("heat_map", example_tiff)
+
+biab_error_stop("Stop it!")
 
 example_json = file.path(outputFolder, "sample.json")
 if(!file.exists(example_json)) {
     download.file("https://gist.githubusercontent.com/wavded/1200773/raw/e122cf709898c09758aecfef349964a8d73a83f3/sample.json", example_json, "auto")
 }
+biab_outputs("geo_json", example_json)
 
 some_csv_data = file.path(outputFolder, "some_data.csv")
 write("Model,mpg,cyl,disp,hp,drat,wt,qsec,vs,am,gear,carb
@@ -35,6 +40,7 @@ Mazda RX4 Wag,21,6,160,110,3.9,2.875,17.02,0,1,4,4
 Datsun 710,22.8,4,108,93,3.85,2.32,18.61,1,1,4,1
 Hornet 4 Drive,21.4,6,258,110,3.08,3.215,19.44,1,0,3,1
 ", some_csv_data)
+biab_outputs("some_csv_data", some_csv_data)
 
 # Example from https://en.wikipedia.org/wiki/Tab-separated_values
 some_tsv_data = file.path(outputFolder, "some_data.tsv")
@@ -45,6 +51,7 @@ write("Sepal length	Sepal width	Petal length	Petal width	Species
 4.6	3.1	1.5	0.2	I. setosa
 5.0	3.6	1.4	0.2	I. setosa
 ", some_tsv_data)
+biab_outputs("some_tsv_data", some_tsv_data)
 
 # Example from https://en.wikipedia.org/wiki/Tab-separated_values
 some_html_output = file.path(outputFolder, "some_page.html")
@@ -52,24 +59,19 @@ write(
     "<h1>This is a page</h1><p>With a fancy interactive graph or the like.</p>",
     some_html_output
 )
+biab_outputs("some_html_output" = some_html_output)
 
 ## Outputing result to JSON
 # notice that the warning string is not part of the yml spec, so it cannot be used by other scripts, but will still be displayed.
-output <- list(#"error" = "Some error", # Use error key to stop the rest of the pipeline
-                "info" = "Some information message",
-                "warning" = "Some warning",
-                "text" = "This is just an example. In case you have a very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very long text it will need to be unfolded to see it all.",
-                "number" = input$intensity * 3,
-                "heat_map" = example_tiff,
-                "geo_json" = example_json,
-                "some_csv_data" = some_csv_data,
-                "some_tsv_data" = some_tsv_data,
-                "some_html_output" = some_html_output,
-                "some_picture" = example_jpg,
-                "userdata_available" = list.files(file.path(Sys.getenv("USERDATA_LOCATION"))),
-                "undocumented_output" = "Some debug output")
+# output <- list(#"error" = "Some error", # Use error key to stop the rest of the pipeline
+#                 "info" = "Some information message",
+#                 "warning" = "Some warning",
+#                 "text" = "This is just an example. In case you have a very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very long text it will need to be unfolded to see it all.",
+#                 "number" = input$intensity * 3,
+#                 "userdata_available" = list.files(file.path(Sys.getenv("USERDATA_LOCATION"))),
+#                 "undocumented_output" = "Some debug output")
 
-jsonData <- toJSON(output, indent=2)
-write(jsonData, file.path(outputFolder,"output.json"))
+# jsonData <- toJSON(output, indent=2)
+# write(jsonData, file.path(outputFolder,"output.json"))
 
 # (If we get a problem with encoding, we could use utf-8 library to clean the output, since the server reads it as utf-8)
