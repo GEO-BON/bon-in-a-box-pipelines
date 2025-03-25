bd58e9986a5c622027bc687cfa1c47695291e7e7
JoryGriffith <jory.griffith@gmail.com>
2025-03-25 13:44:43 -0400

    Fixed bbox script

--- scripts/data/getBoundingBox.R
+++ scripts/data/getBoundingBox.R
@@ -2,6 +2,8 @@
 
 library("rjson")
 library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
 
 input <- biab_inputs()
 
@@ -17,26 +19,25 @@ if (is.null(input$studyarea_file)){ # if there is no study area file input
     study_area<- paste0("https://geoio.biodiversite-quebec.ca/state_geojson/?country_name=", input$country, "&state_name=", input$state)
   } } else {study_area <- input$studyarea_file}
 
-study_area_polygon<- sf::st_read(study_area) %>% st_set_crs("EPSG:4326") # load study area as sf object
-print(st_crs(study_area_polygon))
-
+study_area_polygon <- sf::st_read(study_area) 
 if(nrow(study_area_polygon)==0){
   biab_error_stop("Study area polygon does not exist. Check spelling of country and state names.")
-
 }  # stop if object is empty
 
+# Set crs and transform to the input crs
+study_area_polygon <- st_set_crs(x=study_area_polygon, value="EPSG:4326") # load study area as sf object
+
+if (!is.null(input$study_area_epsg)){ # if crs is specified, transform data
+  print("projecting polygon")
+  study_area_polygon <- st_transform(study_area_polygon, input$study_area_epsg) # transform into input crs
+  }
+  
 # Save study area and protected area data
-study_area_polygon_unprojected <- study_area_polygon
-study_area_polygon_path<- file.path(outputFolder, "study_area_polygon_unprojected.geojson") # Define the file path for the protected area polygon output
-sf::st_write(study_area_polygon_unprojected, study_area_polygon_path, delete_dsn = T)
-biab_output("study_area_polygon_unprojected", study_area_polygon_path)
+study_area_polygon<- file.path(outputFolder, "study_area_polygon.gpkg") # Define the file path for the protected area polygon output
+sf::st_write(study_area_polygon, study_area_polygon_path, delete_dsn = T)
+biab_output("study_area_polygon", study_area_polygon_path)
 
-# create bounding box
-if (is.null(input$studyarea_epsg)){
-  bbox <- sf::st_bbox(study_area_polygon)
-} else {
-study_area_polygon_projected <- st_transform(study_area_polygon, input$studyarea_epsg)
-bbox <- sf::st_bbox(study_area_polygon_projected) }
-
+# extract bounding box and create output
+bbox <- sf::st_bbox(study_area_polygon)
 bbox <- unname(bbox)
 biab_output("bbox", bbox)
