ea0e2f937702623b58281d494812e9c707aaa135
JoryGriffith <jory.griffith@gmail.com>
2025-05-09 15:43:29 -0400

    Rearrange inputs

--- scripts/protconn_analysis/protconn_analysis.R
+++ scripts/protconn_analysis/protconn_analysis.R
@@ -18,7 +18,7 @@ print(st_crs(study_area))
 study_area <- st_transform(study_area, st_crs(input$crs))
 
 ### Load protected area shapefile
-if(input$pa_input_type == "WDPA" | input$pa_input_type =="User input"){ # if only using WDPA data or user data, load that
+if(input$pa_input_type == "WDPA"){ # if only using WDPA data, load that
 protected_area <- st_read(input$protected_area_polygon, type=3, promote_to_multi=FALSE) # input as polygons
 protected_area <- st_transform(protected_area, st_crs(input$crs))
 protected_area <- st_make_valid(protected_area)
@@ -30,6 +30,8 @@ protected_area$legal_status_updated_at <- lubridate::year(protected_area$legal_s
 }
 
 if(input$pa_input_type == "User input"){ # rename and parse date column
+protected_area_user <- input$protected_area_polygon %>% filter(grepl("userdata", input$protected_area_polygon)) # make sure it reads the userdata file path
+protected_area <- st_read(protected_area_user, type=3, promote_to_multi=FALSE) # load
 protected_area <- protected_area %>% rename(legal_status_updated_at = input$date_column)
 protected_area$legal_status_updated_at <- lubridate::parse_date_time(protected_area$legal_status_updated_at, orders=c("ymd", "mdy", "dmy", "y"))
 protected_area$legal_status_updated_at <- lubridate::year(protected_area$legal_status_updated_at)
