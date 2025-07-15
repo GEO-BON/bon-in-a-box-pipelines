04eda56cbebc13236096d5bd2fc8731be0d6fdaf
Laetitia Tremblay <laetitiavtremblay@hotmail.com>
2025-07-15 12:48:01 -0400

    Added phenology md file, moved json file, formatting

--- scripts/phenology/phenology_difference.R
+++ scripts/phenology/phenology_difference.R
@@ -10,8 +10,7 @@ rasters <- input$rasters
 
 start_year <- toString(input$start_year)
 
-end_year <- toString(as.integer(input$end_year)-1)
-
+end_year <- toString(as.integer(input$end_year) - 1)
 
 first_raster <- rasters[grepl(start_year, rasters)]
 print(first_raster)
@@ -30,11 +29,11 @@ num_bands <- nlyr(load_end_raster)
 layer_paths <- c()
 
 print(names(load_end_raster))
-for (i in 1:num_bands){
-    phenology_change <- load_end_raster[[i]]-load_first_raster[[i]]
+for (i in 1:num_bands) {
+    phenology_change <- load_end_raster[[i]] - load_first_raster[[i]]
     phenology_change_path <- file.path(outputFolder, paste0(names(phenology_change), "_difference.tif"))
     out <- writeRaster(phenology_change, phenology_change_path)
-    layer_paths[i]<-phenology_change_path
+    layer_paths[i] <- phenology_change_path
 }
 
 biab_output("phenology_change", layer_paths)
@@ -42,17 +41,34 @@ biab_output("phenology_change", layer_paths)
 # Load summary data
 summary.dat <- read.csv(input$timeseries)
 num.col <- ncol(summary.dat)
-
-summary.dat$date = format(as.POSIXct(summary.dat$date,format='%Y-%m-%dT00:00:00.000Z'),format='%Y-%m-%d')
+summary.dat$date <- format(as.POSIXct(summary.dat$date, format = "%Y-%m-%dT00:00:00.000Z"), format = "%Y-%m-%d")
 
 # Pivot
-summary.dat.long <- summary.dat %>% pivot_longer(cols=names(summary.dat[,c(3:num.col)]), names_to = "band", values_to = "summary")
-print("summary.dat.long:")
-print(summary.dat.long)
-# Plot
-phenology_change_plot <- ggplot(summary.dat.long, aes(x = date, y = summary)) +
-    geom_col(fill='#1d7368') +
-    facet_wrap(~band, ncol = 1, scales = "free_y")
+if (num.col > 3) {
+    summary.dat.long <- summary.dat %>% pivot_longer(cols = names(summary.dat[, c(3:num.col)]), names_to = "band", values_to = "summary")
+    print("summary.dat.long:")
+    print(summary.dat.long)
+} else {
+    summary.dat.long <- summary.dat
+    print("summary.dat.long:")
+    print(summary.dat.long)
+}
+
+print(summary.dat.long[3])
+
+if ("band" %in% colnames(summary.dat.long)) {
+    # For multiple bands
+    phenology_change_plot <- ggplot(summary.dat.long, aes(x = date, y = summary)) +
+        geom_col(fill = "#1d7368") +
+        facet_wrap(~ band, ncol = 1, scales = "free_y")
+} else {
+    # For one band
+    band <- colnames(summary.dat.long)[3]
+    phenology_change_plot <- ggplot(summary.dat.long, aes(x = date, y = .data[[band]])) +
+        geom_col(fill = "#1d7368") +
+        labs(y = "summary", title = paste(band)) +
+        theme(plot.title = element_text(hjust = 0.5))
+}
 
 phenology_change_plot_path <- file.path(outputFolder, "phenology_change_plot.png")
 ggsave(phenology_change_plot_path, phenology_change_plot)
