60ac99e1694a9d0aa3755fc1daff20e66b7155c4
JoryGriffith <jory.griffith@gmail.com>
2025-08-07 16:20:09 -0400

    Added subregion output

--- scripts/data/getCountryPolygon.R
+++ scripts/data/getCountryPolygon.R
@@ -4,10 +4,6 @@ library(dplyr)
 library(countrycode)
 library(httr2)
 
-if (!requireNamespace("packageName", quietly = TRUE)) {
-  remotes::install_github("ropensci/rnaturalearthhires")
-}
-
 input <- biab_inputs()
 
 # Output country and region
@@ -25,6 +21,12 @@ if (is.null(input$region)) {
 
 biab_output("region", region)
 
+if (is.null(input$subregion)) {
+  region <- "No sub-region chosen"
+} else {
+  region <- input$region
+}
+
 # Change from ISO code to country namelibrary(countrycode)
 country_name <- countrycode(
   input$country,
@@ -35,18 +37,28 @@ country_name <- countrycode(
 biab_output("country", country_name)
 
 
-if (is.null(input$region)) { # pull study area polygon from rnaturalearth
+if (is.null(input$region) & is.null(input$subregion)) { # pull study area polygon
   # pull whole country
-  res <- request(paste0("https://www.geoboundaries.org/api/current/gbOpen/", input$country, "/ADM0")) |> 
-  req_perform()
-
+  tryCatch({
+  res <- request(paste0("https://www.geoboundaries.org/api/current/gbOpen/", input$country, "/ADM1")) |> 
+    req_perform()
+  }, error = function(e) {
+  if (grepl("404|Not Found", e$message)) {
+    biab_error_stop("Could not find polygon. Check that you have correct country code.")
+  } else {
+    stop(e)  # re-throw any other error
+  }
+})
   meta <- res |> resp_body_json() # parse JSON
 
   geojson_url <- meta$gjDownloadURL # Extract the GeoJSON download URL
 
   country_region_polygon <- st_read(geojson_url) # Load geojson
+if (nrow(country_region_polygon) == 0) {
+  biab_error_stop(paste0("Could not find polygon. Check that you have correct country code. "))
+} # stop if object is empty
   
-} else {
+} else if (is.null(input$subregion)) {
   print("pulling region polygon")
   res <- request(paste0("https://www.geoboundaries.org/api/current/gbOpen/", input$country, "/ADM1")) |> 
   req_perform() # ADM1 gives regions/provinces
@@ -57,13 +69,35 @@ if (is.null(input$region)) { # pull study area polygon from rnaturalearth
 
   country_region_polygon <- st_read(geojson_url)
 
-  print(country_region_polygon$shapeISO)
-  country_region_polygon <- country_region_polygon[country_region_polygon$shapeISO == input$region, ] # filter shape by region of interest
-}
+  print(country_region_polygon$shapeName)
+  shape_names_str <- paste(country_region_polygon$shapeName, collapse = ", ")
+  country_region_polygon <- country_region_polygon[country_region_polygon$shapeName == input$region, ] # filter shape by region of interest
+if (nrow(country_region_polygon) == 0) {
+  print(shape_names_str)
+  biab_error_stop(paste0("Could not find polygon. Check that you have correct country codes and region names. Valid inputs are: ", shape_names_str))
+} # stop if object is empty
+
+
+} else {
+  res <- request(paste0("https://www.geoboundaries.org/api/current/gbOpen/", input$country, "/ADM2")) |> 
+  req_perform() # ADM2 gives subregions
+
+  meta <- res |> resp_body_json() 
+
+  geojson_url <- meta$gjDownloadURL 
+
+  country_region_polygon <- st_read(geojson_url)
+
+  print(country_region_polygon$shapeName)
+  shape_names_str <- paste(country_region_polygon$shapeName, collapse = ", ")
+  country_region_polygon <- country_region_polygon[country_region_polygon$shapeName == input$subregion, ] # filter shape by region of interest
 
 if (nrow(country_region_polygon) == 0) {
-  biab_error_stop("Could not find polygon. Check that you have correct country and region codes. If inputing region codes, check logs for a list of valid codes.")
+  biab_error_stop(paste0("Could not find polygon. Check that you have correct country codes and region names. Valid inputs are: ", shape_names_str))
 } # stop if object is empty
+
+}
+
 
 # transform to crs of interest
 country_region_polygon <- st_transform(country_region_polygon, crs = input$crs)
