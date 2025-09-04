ca67ddec3209aa4ed2f628a4f232debd6ee3832a
Laetitia Tremblay <laetitiavtremblay@hotmail.com>
2025-09-04 13:24:12 -0400

    fixed merging error

--- scripts/data/getCountryPolygon.R
+++ scripts/data/getCountryPolygon.R
@@ -10,6 +10,13 @@ if (!requireNamespace("packageName", quietly = TRUE)) {
 
 input <- biab_inputs()
 
+# Output country and region
+if (is.null(input$country)) {
+  country <- "No country chosen"
+} else {
+  country <- input$country
+}
+
 if (is.null(input$region)) {
   region <- "No region chosen"
 } else {
@@ -18,17 +25,14 @@ if (is.null(input$region)) {
 
 biab_output("region", region)
 
-if (!is.null(input$country)) {
-  country <- input$country
+# Change from ISO code to country namelibrary(countrycode)
+country_name <- countrycode(
+  input$country,
+  origin = "iso3c",
+  destination = "country.name.en"
+)
 
-  # Change from ISO code to country namelibrary(countrycode)
-  country_name <- countrycode(
-    input$country,
-    origin = "iso3c",
-    destination = "country.name.en"
-  )
-
-  biab_output("country", country_name)
+biab_output("country", country_name)
 
 
 if (is.null(input$region)) { # pull study area polygon from rnaturalearth
@@ -36,9 +40,9 @@ if (is.null(input$region)) { # pull study area polygon from rnaturalearth
   res <- request(paste0("https://www.geoboundaries.org/api/current/gbOpen/", input$country, "/ADM0")) |>
   req_perform()
 
-    meta <- res |> resp_body_json() # parse JSON
+  meta <- res |> resp_body_json() # parse JSON
 
-    geojson_url <- meta$gjDownloadURL # Extract the GeoJSON download URL
+  geojson_url <- meta$gjDownloadURL # Extract the GeoJSON download URL
 
   country_region_polygon <- st_read(geojson_url) # Load geojson
 
@@ -56,3 +60,19 @@ if (is.null(input$region)) { # pull study area polygon from rnaturalearth
   print(country_region_polygon$shapeISO)
   country_region_polygon <- country_region_polygon[country_region_polygon$shapeISO == input$region, ] # filter shape by region of interest
 }
+
+if (nrow(country_region_polygon) == 0) {
+  biab_error_stop("Could not find polygon. Check that you have correct country and region codes. If inputing region codes, check logs for a list of valid codes.")
+} # stop if object is empty
+
+# transform to crs of interest
+country_region_polygon <- st_transform(country_region_polygon, crs = input$crs)
+print(st_crs(country_region_polygon))
+
+print("Study area downloaded")
+print(class(country_region_polygon))
+
+# output country polygon
+country_region_polygon_path <- file.path(outputFolder, "country_region_polygon.gpkg")
+sf::st_write(country_region_polygon, country_region_polygon_path, delete_dsn = T)
+biab_output("country_region_polygon", country_region_polygon_path)
