1fc64379ee3d22de222fe62f0d0c3d7e9717ee78
Jean-Michel Lord <jean-michel.lord@mcgill.ca>
2025-09-04 16:56:22 -0400

    hpc demo script

---
+++ scripts/helloWorld/helloHPC.py
@@ -0,0 +1,23 @@
+import sys, json;
+import time;
+
+# Reading inputs
+data = biab_inputs()
+intIn = data['some_int']
+
+# Do stuff.
+print("Will start when counter reaches input value, so you see logs go by...")
+counter = 0
+for x in range(0, intIn + 1):
+  print(x, flush=True)
+  time.sleep(0.5)
+print("Go!", flush=True)
+
+if intIn == 13 :
+  biab_error_stop("intIn == 13, you're not lucky! This causes failure.")
+  print("You will never see this message")
+
+intIn += 1
+
+# Saving result
+biab_output("increment", intIn)
