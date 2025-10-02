1252fc2d45029cc3d4e38889dd0d6f8984e9f639
Jean-Michel Lord (Arbutus) <jean-michel.lord@mcgill.ca>
2025-10-02 19:22:17 +0000

    helloHPC to test patial jobs

--- scripts/helloWorld/helloHPC.py
+++ scripts/helloWorld/helloHPC.py
@@ -3,7 +3,7 @@ import time;
 
 # Reading inputs
 data = biab_inputs()
-intIn = data['some_int']
+seconds = data['seconds']
 fileIn = data['some_csv_file']
 
 # Validations
@@ -11,23 +11,23 @@ if not os.path.exists(fileIn):
   biab_error_stop(f"File '{fileIn}' does not exist.")
 
 # Do stuff.
-print("Will start when counter reaches input value, so you see logs go by...")
+biab_output("target", seconds)
+
+print("Counting the seconds...")
 counter = 0
-for x in range(0, intIn + 1):
+for x in range(0, seconds + 1):
   print(x, flush=True)
-  time.sleep(0.5)
-print("Go!", flush=True)
+  time.sleep(1)
 
-if intIn == 13 :
-  biab_error_stop("intIn == 13, you're not lucky! This causes failure.")
+print("Done!", flush=True)
+
+if seconds == 13 :
+  biab_error_stop("seconds == 13, you're not lucky! This causes failure.")
   print("You will never see this message")
-
-intIn += 1
 
 with open(fileIn, 'r') as f:
   content = f.read()
   print("Contents of csv input:")
   print(content)
 
-# Saving result
-biab_output("increment", intIn)
+  biab_output("length", len(content))
