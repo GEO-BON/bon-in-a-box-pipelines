8021c17383d4597520df1e00af4551a3591fb803
JoryGriffith <83308254+JoryGriffith@users.noreply.github.com>
2025-12-10 11:58:44 -0500

    Tried to add polygon filter to wdpa (doesn;t work right now)

--- scripts/data/load_polygons.R
+++ scripts/data/load_polygons.R
@@ -4,7 +4,11 @@ library(dplyr)
 if (!require("duckdbfs")) {
     install.packages("duckdbfs")
 }
+if (!require("duckspatial")) {
+    install.packages("duckspatial")
+}
 library(duckdbfs)
+library(duckspatial)
 
 input <- biab_inputs()
 
@@ -20,7 +24,8 @@ if (input$polygon_type == "Country or region" | input$polygon_type == "WDPA") {
     } else if (is.null(input$country_region$country) & is.null(input$country_region$region)) { # if both are null (custom bounding box)
         bbox_values <- (input$bbox_crs$bbox)
         names(bbox_values) <- c("xmin", "ymin", "xmax", "ymax")
-        geo_data_sf <- st_as_sfc(st_bbox(bbox_values, crs = crs))
+        geo_data_sf <- st_as_sfc(st_bbox(bbox_values, crs = crs)) ### need to assign CRS
+        # transform back to 4326 if not already NEED TO DO THIS
     } else { # if there is country and region
         country_region <- open_dataset("https://data.fieldmaps.io/edge-matched/humanitarian/intl/adm1_polygons.parquet")
         geo_data_sf <- country_region |>
@@ -33,12 +38,27 @@ if (input$polygon_type == "Country or region" | input$polygon_type == "WDPA") {
 }
 
 if (input$polygon_type == "WDPA") {
-    country_region <- geo_data_sf
-    wdpa <- open_dataset("https://object-arbutus.cloud.computecanada.ca/bq-io/vectors-cloud/wdpa/wdpa.parquet")
-    geo_data_sf <- wdpa |> # change this to st_within to allow for transboundary buffer
-            filter(ISO3 == input$country_region$country$ISO3) |> 
-            to_sf() |>
-            st_set_crs(4326)
+country_region_polygon <- geo_data_sf
+print(colnames(country_region_polygon))
+url <- "https://object-arbutus.cloud.computecanada.ca/bq-io/vectors-cloud/wdpa/wdpa.parquet"
+
+con <- dbConnect(duckdb())
+
+dbExecute(con,"INSTALL spatial; INSTALL httpfs; LOAD spatial; LOAD httpfs;")
+
+ddbs_write_vector(
+con,
+country_region_polygon,
+"region",
+overwrite = TRUE
+)
+
+buffer <- input$buffer
+
+wdpa_area<-dbExecute(con,paste0("CREATE OR REPLACE VIEW tmp AS SELECT w.* FROM read_parquet('",url,"') w, region WHERE st_intersects(w.SHAPE,region.geom)"))
+
+geo_data_sf <- ddbs_read_vector(con, "tmp") |> st_set_crs(4326)
+print(geo_data_sf)
 }
 
 if (input$polygon_type == "EEZ") {
@@ -55,6 +75,8 @@ if (input$polygon_type == "EEZ") {
         biab_error_stop("There is no Exclusive Economic Zone for this country")
     }
 }
+
+# Transform to crs of interest
 
 polygon_path <- file.path(outputFolder, "polygon.gpkg")
 st_write(geo_data_sf, polygon_path)
