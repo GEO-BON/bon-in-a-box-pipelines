58d4c8360934838e43dbda723fc755e5ce1999df
JoryGriffith <jory.griffith@gmail.com>
2026-01-07 16:28:55 -0500

    fix gee script so it accepts polygons

--- scripts/data/loadGEEdata.py
+++ scripts/data/loadGEEdata.py
@@ -24,19 +24,35 @@ gee_layer_name = inputs['gee_layer_name']
 hab = ee.Image(gee_layer_name)
 
 #Either load bounding box or polygon
+gdf = gpd.read_file(inputs['polygon'])
 
-polygon = ee.Geometry.Rectangle([-80.60, 20.40, -72.50, 27.50], proj='EPSG:4326', geodesic=False)
-# if bounding box
-#if input['polygon'] is None:
-    #polygon = ee.Geometry.Rectangle(input['bbox'], proj=input['proj'], geodesic=False)
-# if polygon
-#else:
- #   polygon = polygon = Polygon(input['polygon'])
+geom = gdf.geometry.iloc[0]
+print(geom)
+
+features = []
+for geom in gdf.geometry:
+    if geom.geom_type == "Polygon":
+        rings = [list(geom.exterior.coords)] + [
+            list(r.coords) for r in geom.interiors
+        ]
+        features.append(ee.Feature(ee.Geometry.Polygon(rings)))
+
+    elif geom.geom_type == "MultiPolygon":
+        for poly in geom.geoms:
+            rings = [list(poly.exterior.coords)] + [
+                list(r.coords) for r in poly.interiors
+            ]
+            features.append(ee.Feature(ee.Geometry.Polygon(rings)))
+
+ee_fc = ee.FeatureCollection(features)
+
+
+ee_fc = ee.FeatureCollection(features)
 
 # -----------------------------
 # 4. Clip habitat to EEZ
 # -----------------------------
-hab_clipped = hab.clip(polygon)
+hab_clipped = hab.clip(ee_fc)
 print(hab_clipped)
 
 bands = inputs['bands']
@@ -47,7 +63,6 @@ hab_clipped = hab_clipped.select(bands)
 # 5. Export/download image locally
 # -----------------------------
 
-# Use ee.batch.Export.image.toDrive() for large images
 outfile = ("%s/gee_layer.tif") % (output_folder)
 
 resolution = inputs['resolution']
