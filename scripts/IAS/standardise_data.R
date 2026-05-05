8caecbb99e862cb279896df9153426dac8f48553
Samara Manzin <samaramanzin@gmail.com>
2026-05-05 09:58:52 -0400

    Updaing standardise data script

--- scripts/IAS/standardise_data.R
+++ scripts/IAS/standardise_data.R
@@ -1,6 +1,9 @@
 library(jsonlite)
-library(data.table)
 library(duckdb)
+library(dplyr)
+
+input <- biab_inputs()
+country_name <- input$country_name$country$englishName
 
 # First record data doi
 doi <- "https://doi.org/10.5281/zenodo.18220953"
@@ -27,3 +30,38 @@ target_url <- files_df$direct_link[grepl("\\.csv$", files_df$filename)]
 # parsing by country "location" because the file is so big
 con <- dbConnect(duckdb())
 dbExecute(con, "INSTALL httpfs; LOAD httpfs;")
+
+query <- paste0("
+  SELECT * FROM read_csv_auto('", target_url, "', delim=' ', header=True)
+  WHERE location = country_name
+")
+
+first_records <- dbGetQuery(con, query)
+
+# Close connection
+dbDisconnect(con, shutdown = TRUE)
+
+# Standardise habitat terms
+first_records <- first_records %>% dplyr::mutate(habitat = toupper(habitat)) %>% 
+  dplyr::mutate(habitat = gsub(";","|",habitat)) %>%
+  dplyr::mutate(habitat = dplyr::case_when(habitat == "" ~ "NODATA", TRUE ~ as.character(habitat))) 
+
+UniqueHabitats <- first_records %>% 
+  dplyr::mutate(habitat = gsub(";","|",habitat)) %>%
+  dplyr::distinct(habitat) %>% 
+  dplyr::arrange() %>%
+  dplyr::mutate(habitat = dplyr::case_when(habitat == "" ~ "NODATA", TRUE ~ as.character(habitat))) %>%
+  dplyr::mutate(terrestrial = dplyr::case_when(grepl(c("TERRESTRIAL"), habitat) ~ "TERRESTRIAL ", TRUE ~ ""),
+                marine = dplyr::case_when(grepl("MARINE", habitat) ~ "MARINE ", TRUE ~ ""),
+                freshwater = dplyr::case_when(grepl(c("FRESHWATER"), habitat) ~ "FRESHWATER ", TRUE ~ ""),
+                brackish = dplyr::case_when(grepl("BRACKISH", habitat) ~ "BRACKISH ", TRUE ~ ""),
+                nodata = dplyr::case_when(grepl("NODATA", habitat) ~ "NODATA ", TRUE ~ "")) %>% 
+  dplyr::mutate(habitatStandardised = paste(terrestrial,marine,freshwater,brackish,nodata)) %>% 
+  dplyr::mutate(habitatStandardised = stringr::str_trim(habitatStandardised, side = "both")) %>%
+  dplyr::mutate(habitatStandardised = stringr::str_squish(habitatStandardised)) %>%
+  dplyr::mutate(habitatStandardised = gsub(" ","|",habitatStandardised)) %>% 
+  dplyr::select(habitat,habitatStandardised) 
+
+FirstRecords <- dplyr::left_join(first_records, UniqueHabitats, by = "habitat") %>% 
+  dplyr::select(-habitat) %>% 
+  dplyr::rename(habitat = habitatStandardised)
