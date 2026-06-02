6a078a0278b6a1d32e4a8fe61fb64af157d3decb
Samara Manzin <samaramanzin@gmail.com>
2026-06-02 13:54:56 -0400

    updating iucn esa conversion with new table from Santini et al (2019)

--- scripts/SHI/esa_conversion.R
+++ scripts/SHI/esa_conversion.R
@@ -15,15 +15,12 @@ if (token == "") {
   biab_error_stop("Please specify an IUCN token in your environment file")
 }
 
-# Format conversion table
-conversion_table <- read.csv(conversion_table)
-conversion_table_tidy <- conversion_table |> dplyr::count(HABITAT_IUCN, ESA_cod, ESA_cov) |> 
-  separate_rows(HABITAT_IUCN, sep="; ") |> 
-  mutate(IUCN_code = gsub("\\.$" , "" , str_extract( HABITAT_IUCN, "\\d*\\.*\\d*")) , .before = HABITAT_IUCN) |> 
-  mutate(IUCN_code = gsub("\\.", "_", IUCN_code)) |>
-  select(-n)
+conversion_table <- "scripts\\SHI\\iucn_esa_conversion.csv"    
+conversion_table <- read.csv(conversion_table) |>
+  mutate(IUCN_code = as.character(IUCN_code)) |>
+  mutate(IUCN_code = gsub(".", "_", IUCN_code, fixed = TRUE)) # change to same format as IUCN code is read in
 
-# Get habitat codes for each species
+# Get habitat codes for each species from IUCN
 habitat_codes <- list()
 for (i in 1:length(species)) {
   print(paste("Processing species", species[i]))
@@ -34,10 +31,11 @@ species_species <- str_split(species[i], " ")[[1]][2]
 species_habitats <- rredlist::rl_species_latest(genus = species_genus, species = species_species, key = token)$habitats
 
 # Use conversion table to get ESA habitat codes for each species
-habitat_codes_sp <- conversion_table_tidy |> 
+habitat_codes_sp <- conversion_table |> 
   mutate(species = species[i]) |>
   filter(IUCN_code %in% species_habitats$code) |> 
-  select(species, ESA_cod, ESA_cov)
+  select(species, ESA_CCI_code) |>
+  distinct()
 
 habitat_codes[[i]] <- habitat_codes_sp
 }
@@ -48,13 +46,6 @@ habitat_codes_df <- bind_rows(habitat_codes)
 # Save table as csv
 habitat_codes_path <- file.path(outputFolder, "habitat_codes.csv)")
 write.csv(habitat_codes_df, habitat_codes_path, row.names = FALSE)
-
-# habitat codes in conversion table not found in species_habitats
-missing_codes <- species_habitats |>
-  distinct(code) |>
-  filter(!code %in% conversion_table_tidy$IUCN_code)
-
-nrow(missing_codes)
 
 # output results
 biab_output("habitats", habitat_codes_path)
